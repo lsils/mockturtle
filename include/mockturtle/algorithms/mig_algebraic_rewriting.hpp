0a7647fdf397dbc4862962d641aa80233c05b729
Mathias Soeken <mathias.soeken@epfl.ch>
2018-05-15 17:22:57 +0200

    MIG algebraic rewriting.

---
+++ include/mockturtle/algorithms/mig_algebraic_rewriting.hpp
@@ -0,0 +1,142 @@
+/* mockturtle: C++ logic network library
+ * Copyright (C) 2018  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*!
+  \file mig_algebraic_rewriting.hpp
+  \brief MIG algebraric rewriting
+
+  \author Mathias Soeken
+*/
+
+#pragma once
+
+#include <iostream>
+#include <optional>
+
+#include "../views/topo_view.hpp"
+
+namespace mockturtle
+{
+
+struct mig_algebraric_rewriting_params
+{
+};
+
+namespace detail
+{
+
+template<class Ntk>
+class mig_algebraic_rewriting_impl
+{
+public:
+  mig_algebraic_rewriting_impl( Ntk& ntk, mig_algebraric_rewriting_params const& ps )
+      : ntk( ntk ),
+        ps( ps )
+  {
+  }
+
+  void run()
+  {
+    topo_view topo{ntk};
+    topo.foreach_node( [this]( auto n ) {
+      if ( auto cand = associativity_candidate( n ); cand )
+      {
+        const auto& [x, y, z, u] = *cand;
+        auto opt = ntk.create_maj( z, u, ntk.create_maj( x, y, u ) );
+        if ( ntk.is_complemented( opt ) )
+        {
+          std::cout << "BIG PROBLEMS!\n";
+        }
+        ntk.substitute_node( n, ntk.get_node( opt ) );
+        ntk.update();
+      }
+    } );
+  }
+
+private:
+  std::optional<std::array<signal<Ntk>, 4>> associativity_candidate( node<Ntk> const& n ) const
+  {
+    if ( ntk.level( n ) == 0 )
+      return std::nullopt;
+
+    const auto ocs = ordered_children( n );
+    if ( ntk.is_complemented( ocs[2] ) )
+      return std::nullopt;
+
+    if ( ntk.level( ntk.get_node( ocs[2] ) ) <= ntk.level( ntk.get_node( ocs[1] ) ) + 1 )
+      return std::nullopt;
+
+    const auto ocs2 = ordered_children( ntk.get_node( ocs[2] ) );
+
+    if ( ntk.level( ntk.get_node( ocs2[2] ) ) == ntk.level( ntk.get_node( ocs2[1] ) ) )
+      return std::nullopt;
+
+    if ( ocs[0] == ocs2[0] )
+    {
+      return std::array<signal<Ntk>, 4>{ocs[1], ocs2[1], ocs2[2], ocs[0]};
+    }
+    if ( ocs[0] == ocs2[1] )
+    {
+      return std::array<signal<Ntk>, 4>{ocs[1], ocs2[0], ocs2[2], ocs[0]};
+    }
+    if ( ocs[1] == ocs2[0] )
+    {
+      return std::array<signal<Ntk>, 4>{ocs[0], ocs2[1], ocs2[2], ocs[1]};
+    }
+    if ( ocs[1] == ocs2[1] )
+    {
+      return std::array<signal<Ntk>, 4>{ocs[0], ocs2[0], ocs2[2], ocs[1]};
+    }
+
+    return std::nullopt;
+  }
+
+  std::array<signal<Ntk>, 3> ordered_children( node<Ntk> const& n ) const
+  {
+    std::array<signal<Ntk>, 3> children;
+    ntk.foreach_fanin( n, [&children]( auto const& f, auto i ) { children[i] = f; } );
+    std::sort( children.begin(), children.end(), [this]( auto const& c1, auto const& c2 ) {
+      return ntk.level( ntk.get_node( c1 ) ) < ntk.level( ntk.get_node( c2 ) );
+    } );
+    return children;
+  }
+
+private:
+  Ntk& ntk;
+  mig_algebraric_rewriting_params const& ps;
+};
+
+} // namespace detail
+
+template<class Ntk>
+void mig_algebraic_rewriting( Ntk& ntk, mig_algebraric_rewriting_params const& ps = {} )
+{
+  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
+
+  detail::mig_algebraic_rewriting_impl<Ntk> p( ntk, ps );
+  p.run();
+}
+
+} /* namespace mockturtle */
---
+++ test/algorithms/mig_algebraic_rewriting.cpp
@@ -0,0 +1,32 @@
+#include <catch.hpp>
+
+#include <mockturtle/traits.hpp>
+#include <mockturtle/algorithms/mig_algebraic_rewriting.hpp>
+#include <mockturtle/networks/mig.hpp>
+#include <mockturtle/views/depth_view.hpp>
+
+using namespace mockturtle;
+
+TEST_CASE( "MIG depth optimization", "[mig_algebraic_rewriting]" )
+{
+  mig_network mig;
+
+  const auto a = mig.create_pi();
+  const auto b = mig.create_pi();
+  const auto c = mig.create_pi();
+  const auto d = mig.create_pi();
+
+  const auto f1 = mig.create_and( a, b );
+  const auto f2 = mig.create_and( f1, c );
+  const auto f3 = mig.create_and( f2, d );
+
+  mig.create_po( f3 );
+
+  depth_view depth_mig{mig};
+
+  CHECK( depth_mig.depth() == 3 );
+
+  mig_algebraic_rewriting( depth_mig );
+
+  CHECK( depth_mig.depth() == 2 );
+}
