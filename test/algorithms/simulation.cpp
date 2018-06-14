afa96985d03ead7ac2f39007cb807ad3e8a79cca
Mathias Soeken <mathias.soeken@epfl.ch>
2018-06-14 11:24:04 +0200

    Simulation test.

--- test/algorithms/simulation.cpp
+++ test/algorithms/simulation.cpp
@@ -25,6 +25,23 @@ TEST_CASE( "Simulate XOR AIG circuit with Booleans", "[simulation]" )
   CHECK( !simulate<bool>( aig, default_simulator<bool>( {false, false} ) )[0] );
 }
 
+TEST_CASE( "Simulate XOR AIG circuit with static truth table", "[simulation]" )
+{
+  aig_network aig;
+
+  const auto a = aig.create_pi();
+  const auto b = aig.create_pi();
+  const auto f1 = aig.create_nand( a, b );
+  const auto f2 = aig.create_nand( a, f1 );
+  const auto f3 = aig.create_nand( b, f1 );
+  const auto f4 = aig.create_nand( f2, f3 );
+  aig.create_po( f4 );
+
+  const auto tt = simulate<kitty::static_truth_table<2>>( aig )[0];
+  CHECK( tt._bits == 0x6 );
+}
+
+
 TEST_CASE( "Simulate XOR AIG circuit with dynamic truth table", "[simulation]" )
 {
   aig_network aig;
