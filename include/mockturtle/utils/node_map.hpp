c71ea8ccf110863aa429f6533bb53d54856c2c77
Mathias Soeken <mathias.soeken@epfl.ch>
2018-06-14 09:01:58 +0200

    Fix to node map.

--- include/mockturtle/utils/node_map.hpp
+++ include/mockturtle/utils/node_map.hpp
@@ -71,6 +71,9 @@ template<class T, class Ntk>
 class node_map
 {
 public:
+  using reference = typename std::vector<T>::reference;
+  using const_reference = typename std::vector<T>::const_reference;
+public:
   /*! \brief Default constructor. */
   explicit node_map( Ntk const& ntk )
       : ntk( ntk ),
@@ -97,13 +100,13 @@ public:
   }
 
   /*! \brief Mutable access to value by node. */
-  auto& operator[]( node<Ntk> const& n )
+  reference operator[]( node<Ntk> const& n )
   {
     return data[ntk.node_to_index( n )];
   }
 
   /*! \brief Constant access to value by node. */
-  auto const& operator[]( node<Ntk> const& n ) const
+  const_reference operator[]( node<Ntk> const& n ) const
   {
     return data[ntk.node_to_index( n )];
   }
@@ -114,7 +117,7 @@ public:
    * are the same in the network implementation, this method is disabled.
    */
   template<typename _Ntk = Ntk, typename = std::enable_if_t<!std::is_same_v<signal<_Ntk>, node<_Ntk>>>>
-  auto& operator[]( signal<Ntk> const& f )
+  reference operator[]( signal<Ntk> const& f )
   {
     return data[ntk.node_to_index( ntk.get_node( f ) )];
   }
@@ -125,7 +128,7 @@ public:
    * are the same in the network implementation, this method is disabled.
    */
   template<typename _Ntk = Ntk, typename = std::enable_if_t<!std::is_same_v<signal<_Ntk>, node<_Ntk>>>>
-  auto const& operator[]( signal<Ntk> const& f ) const
+  const_reference operator[]( signal<Ntk> const& f ) const
   {
     return data[ntk.node_to_index( ntk.get_node( f ) )];
   }
