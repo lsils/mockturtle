c960db838756cd4fde97336c3a2c1cc31bb8f2a2
Heinz Riener <heinz.riener@gmail.com>
2018-07-29 12:16:14 +0200

    update lorina.

--- lib/lorina/lorina/verilog.hpp
+++ lib/lorina/lorina/verilog.hpp
@@ -284,9 +284,9 @@ public:
 
 namespace verilog_regex
 {
-static std::regex immediate_assign( R"(^(~)?([[:digit:][:alpha:]\[\]_]+)$)" );
-static std::regex binary_expression( R"(^(~)?([[:digit:][:alpha:]\[\]_]+)([&|^])(~)?([[:digit:][:alpha:]\[\]_]+)$)" );
-static std::regex maj3_expression( R"(^\((~)?([[:alnum:]\[\]_]+)&(~)?([[:alnum:]\[\]_]+)\)\|\((~)?([[:alnum:]\[\]_]+)&(~)?([[:alnum:]\[\]_]+)\)\|\((~)?([[:alnum:]\[\]_]+)&(~)?([[:alnum:]\[\]_]+)\)$)" );
+static std::regex immediate_assign( R"(^(~)?([[:alnum:]\[\]_']+)$)" );
+static std::regex binary_expression( R"(^(~)?([[:alnum:]\[\]_']+)([&|^])(~)?([[:alnum:]\[\]_']+)$)" );
+static std::regex maj3_expression( R"(^\((~)?([[:alnum:]\[\]_']+)&(~)?([[:alnum:]\[\]_']+)\)\|\((~)?([[:alnum:]\[\]_']+)&(~)?([[:alnum:]\[\]_']+)\)\|\((~)?([[:alnum:]\[\]_']+)&(~)?([[:alnum:]\[\]_']+)\)$)" );
 } // namespace verilog_regex
 
 class verilog_parser
