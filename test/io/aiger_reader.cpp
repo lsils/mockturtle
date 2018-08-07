185e9ef5ebc950c2e67c310b5b3c650b0785e1bc
Heinz Riener <heinz.riener@gmail.com>
2018-08-07 10:55:30 +0200

    aiger_reader.

--- test/io/aiger_reader.cpp
+++ test/io/aiger_reader.cpp
@@ -10,7 +10,7 @@
 
 using namespace mockturtle;
 
-TEST_CASE( "read an ASCII Aiger file into an AIG network", "[aiger_reader]" )
+TEST_CASE( "read an ASCII Aiger file into an AIG network and store input-output names", "[aiger_reader]" )
 {
   aig_network aig;
 
@@ -21,18 +21,31 @@ TEST_CASE( "read an ASCII Aiger file into an AIG network", "[aiger_reader]" )
   "6 2 4\n"
   "8 2 7\n"
   "10 4 7\n"
-  "12 9 11\n"};
+  "12 9 11\n"
+  "i0 foo\n"
+  "i1 bar\n"
+  "o0 foobar\n"};
 
+  std::unordered_map<aig_network::signal,std::string> names;
   std::istringstream in( file );
-  lorina::read_ascii_aiger( in, aiger_reader( aig ) );
+  lorina::read_ascii_aiger( in, aiger_reader( aig, &names ) );
 
   CHECK( aig.size() == 7 );
   CHECK( aig.num_pis() == 2 );
   CHECK( aig.num_pos() == 1 );
   CHECK( aig.num_gates() == 4 );
 
+  aig.foreach_pi( [&]( auto const& n, auto index ) {
+      auto const s = aig.make_signal( n );
+      if ( index == 0 )
+        CHECK( names[s] == "foo" );
+      else if ( index == 1 )
+        CHECK( names[s] == "bar" );
+    } );
+
   aig.foreach_po( [&]( auto const& f ) {
-    CHECK( aig.is_complemented( f ) );
-    return false;
-  } );
+      CHECK( aig.is_complemented( f ) );
+      CHECK( names[f] == "foobar" );
+      return false;
+    } );
 }
