1230e956dbb6fc56070427f1d60a32c0873a7917
Heinz Riener <heinz.riener@gmail.com>
2018-09-09 23:14:58 +0200

    random_logic_generator.

--- test/generators/random_logic_network.cpp
+++ test/generators/random_logic_network.cpp
@@ -6,7 +6,7 @@ using namespace mockturtle;
 
 TEST_CASE( "create random aig_network", "[random_logic_generator]" )
 {
-  random_logic_generator<aig_network> gen;
+  auto const gen = default_random_aig_generator();
   auto const aig = gen.generate( 4u, 100u );
 
   CHECK( aig.num_pis() == 4u );
@@ -15,10 +15,18 @@ TEST_CASE( "create random aig_network", "[random_logic_generator]" )
 
 TEST_CASE( "create random mig_network", "[random_logic_generator]" )
 {
-  random_logic_generator<mig_network> gen;
+  auto const gen = default_random_mig_generator();
   auto const mig = gen.generate( 4u, 100u );
 
   CHECK( mig.num_pis() == 4u );
   CHECK( mig.num_gates() == 100u );
 }
 
+TEST_CASE( "create random mig_network with un-real majority nodes", "[random_logic_generator]" )
+{
+  auto const gen = default_random_mixed_mig_generator();
+  auto const mig = gen.generate( 4u, 100u );
+
+  CHECK( mig.num_pis() == 4u );
+  CHECK( mig.num_gates() == 100u );
+}
