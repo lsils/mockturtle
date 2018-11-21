210f136adfc7da5c987654edd03e095c3f06f8f0
Mathias Soeken <mathias.soeken@epfl.ch>
2018-11-21 17:22:42 +0100

    Performance improvements and statistic recording.

--- include/mockturtle/algorithms/xag_minmc_resynthesis.hpp
+++ include/mockturtle/algorithms/xag_minmc_resynthesis.hpp
@@ -40,7 +40,9 @@
 #include <vector>
 
 #include "../algorithms/cleanup.hpp"
+#include "../utils/stopwatch.hpp"
 #include <kitty/constructors.hpp>
+#include <kitty/hash.hpp>
 #include <kitty/print.hpp>
 #include <kitty/spectral.hpp>
 #include <mockturtle/io/write_bench.hpp>
@@ -53,31 +55,92 @@
 namespace mockturtle
 {
 
+struct xag_minmc_resynthesis_params
+{
+  bool print_stats{true};
+};
+
+struct xag_minmc_resynthesis_stats
+{
+  stopwatch<>::duration time_total{0};
+  stopwatch<>::duration time_parse_db{0};
+  stopwatch<>::duration time_classify{0};
+  stopwatch<>::duration time_cleanup{0};
+
+  uint32_t cache_hits{0};
+  uint32_t cache_misses{0};
+  uint32_t classify_aborts{0};
+  uint32_t unknown_function_aborts{0};
+
+  void report() const
+  {
+    std::cout << fmt::format( "[i] total time    = {:>5.2f} secs\n", to_seconds( time_total ) );
+    std::cout << fmt::format( "[i] parse db time = {:>5.2f} secs\n", to_seconds( time_parse_db ) );
+    std::cout << fmt::format( "[i] classify time = {:>5.2f} secs\n", to_seconds( time_classify ) );
+    std::cout << fmt::format( "[i] - aborts      = {:>5}\n", classify_aborts );
+    std::cout << fmt::format( "[i] cleanup time  = {:>5.2f} secs\n", to_seconds( time_cleanup ) );
+    std::cout << fmt::format( "[i] cache hits    = {:>5}\n", cache_hits );
+    std::cout << fmt::format( "[i] cache misses  = {:>5}\n", cache_misses );
+    std::cout << fmt::format( "[i] unknown func. = {:>5}\n", unknown_function_aborts );
+  }
+};
+
 class xag_minmc_resynthesis
 {
 public:
-  xag_minmc_resynthesis( std::string filename )
+  xag_minmc_resynthesis( std::string const& filename )
   {
     db = std::make_shared<xag_network>();
-    func_mc = std::make_shared<std::unordered_map<std::string, std::tuple<std::string, unsigned, xag_network::signal>>>();
+    db_pis = std::make_shared<decltype( db_pis )::element_type>();
+    func_mc = std::make_shared<decltype( func_mc )::element_type>();
+    classify_cache = std::make_shared<decltype( classify_cache )::element_type>();
     build_db( filename );
   }
 
-  xag_minmc_resynthesis( xag_minmc_resynthesis const& ) = default;
-  xag_minmc_resynthesis( xag_minmc_resynthesis&& ) = default;
-  xag_minmc_resynthesis& operator=( xag_minmc_resynthesis const& ) = default;
-  xag_minmc_resynthesis& operator=( xag_minmc_resynthesis&& ) = default;
-  virtual ~xag_minmc_resynthesis() = default;
+  virtual ~xag_minmc_resynthesis()
+  {
+    if ( ps.print_stats )
+    {
+      st.report();
+    }
+  }
 
   template<typename LeavesIterator, typename Fn>
   void operator()( xag_network& xag, kitty::dynamic_truth_table const& function, LeavesIterator begin, LeavesIterator end, Fn&& fn )
   {
-    kitty::static_truth_table<6> tt_ext;
-    auto spectral = kitty::exact_spectral_canonization_limit( function, 100000 );
+    stopwatch t( st.time_total );
 
-    if ( spectral.second == 0 )
-      return; /* quit */
-    tt_ext = extend_to( spectral.first, 6 );
+    const auto func_ext = kitty::extend_to<6>( function );
+    std::vector<kitty::detail::spectral_operation> trans_2;
+    kitty::static_truth_table<6> tt_ext;
+
+    const auto cache_it = classify_cache->find( func_ext );
+
+    if ( cache_it != classify_cache->end() )
+    {
+      st.cache_hits++;
+      if ( !std::get<0>( cache_it->second ) )
+      {
+        return; /* quit */
+      }
+      tt_ext = std::get<1>( cache_it->second );
+      trans_2 = std::get<2>( cache_it->second );
+    }
+    else
+    {
+      st.cache_misses++;
+      const auto spectral = call_with_stopwatch( st.time_classify, [&]() { return kitty::exact_spectral_canonization_limit( func_ext, 100000, [&trans_2]( auto const& ops ) {
+                                                                             std::copy( ops.begin(), ops.end(),
+                                                                                        std::back_inserter( trans_2 ) );
+                                                                           } ); } );
+      ( *classify_cache )[func_ext] = {spectral.second, spectral.first, trans_2};
+      if ( !spectral.second )
+      {
+        st.classify_aborts++;
+        return; /* quit */
+      }
+      tt_ext = spectral.first;
+    }
 
     unsigned int mc = 0u;
     std::string original_f;
@@ -93,6 +156,7 @@ public:
     }
     else
     {
+      st.unknown_function_aborts++;
       return; /* quit */
     }
 
@@ -102,19 +166,13 @@ public:
     kitty::create_from_hex_string( db_repr, original_f );
     std::vector<xag_network::signal> pis( 6, xag.get_constant( false ) );
     std::copy( begin, end, pis.begin() );
-    
+
     std::vector<kitty::detail::spectral_operation> trans;
-    std::vector<kitty::detail::spectral_operation> trans_2;
-    const auto r1 = kitty::exact_spectral_canonization(
-        db_repr, [&trans]( auto const& ops ) {
-          std::copy( ops.rbegin(), ops.rend(),
-                     std::back_inserter( trans ) );
-        } );
-    const auto r2 = kitty::exact_spectral_canonization(
-        function, [&trans_2]( auto const& ops ) {
-          std::copy( ops.begin(), ops.end(),
-                     std::back_inserter( trans_2 ) );
-        } );
+    call_with_stopwatch( st.time_classify, [&]() { return kitty::exact_spectral_canonization(
+                                                       db_repr, [&trans]( auto const& ops ) {
+                                                         std::copy( ops.rbegin(), ops.rend(),
+                                                                    std::back_inserter( trans ) );
+                                                       } ); } );
 
     for ( auto const& t : trans_2 )
     {
@@ -199,37 +257,36 @@ public:
       }
     }
 
-    auto f = circuit;
-
-    topo_view topo{*db, f};
-    f = cleanup_dangling( topo, xag, pis.begin(), pis.end() ).front(); 
-
-    for ( auto& g : final_xor )
+    xag_network::signal output;
     {
-      f = xag.create_xor( f, g );
+      stopwatch t( st.time_cleanup );
+      cut_view topo{*db, *db_pis, db->get_node( circuit )};
+      output = cleanup_dangling( topo, xag, pis.begin(), pis.end() ).front();
+      if ( db->is_complemented( circuit ) )
+      {
+        output = !output;
+      }
     }
 
-    if ( ( out_neg ) == 1 )
-      fn( !f );
-    else
-      fn( f );
-    return;
-
-    /*if ( !fn( f ) )
+    for ( auto const& g : final_xor )
     {
-      return; /* quit 
-    }*/
+      output = xag.create_xor( output, g );
+    }
+
+    fn( out_neg ? !output : output );
   }
 
 private:
-  void build_db( std::string filename )
+  void build_db( std::string const& filename )
   {
+    stopwatch t( st.time_parse_db );
+
     std::ifstream file1;
     file1.open( filename );
     std::string line;
     for ( auto h = 0; h < 6; h++ )
     {
-      db->create_pi();
+      db_pis->push_back( db->create_pi() );
     }
 
     while ( std::getline( file1, line ) )
@@ -251,7 +308,7 @@ private:
       circuit.erase( 0, circuit.find( delimiter ) + delimiter.length() );
       const auto inputs = std::stoul( token, nullptr, 10 );
 
-      for ( auto h = 0; h < inputs; h++ )
+      for ( auto h = 0u; h < inputs; h++ )
       {
         hashing_circ.push_back( db->make_signal( db->index_to_node( h + 1 ) ) );
       }
@@ -301,7 +358,7 @@ private:
       {
         f = hashing_circ[output / 2 - 1];
       }
-      auto t = db->create_po( f );
+      db->create_po( f );
 
       func_mc->insert( std::make_pair( token_f, std::make_tuple( original, mc, f ) ) );
     }
@@ -309,8 +366,14 @@ private:
     file1.close();
   }
 
+private:
+  xag_minmc_resynthesis_params ps;
+  xag_minmc_resynthesis_stats st;
+
   std::shared_ptr<xag_network> db;
+  std::shared_ptr<std::vector<xag_network::signal>> db_pis;
   std::shared_ptr<std::unordered_map<std::string, std::tuple<std::string, unsigned, xag_network::signal>>> func_mc;
+  std::shared_ptr<std::unordered_map<kitty::static_truth_table<6>, std::tuple<bool, kitty::static_truth_table<6>, std::vector<kitty::detail::spectral_operation>>, kitty::hash<kitty::static_truth_table<6>>>> classify_cache;
 };
 
 } // namespace mockturtle
