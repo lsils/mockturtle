77b86306384bf2e5cdca37149fb1123707094eda
Mathias Soeken <mathias.soeken@epfl.ch>
2018-11-25 14:01:34 +0100

    More test cases.

--- test/networks/aig.cpp
+++ test/networks/aig.cpp
@@ -586,7 +586,7 @@ TEST_CASE( "substitude nodes with propagation in AIGs (test case 2)", "[aig]" )
   CHECK( aig.num_gates() == 1u );
 }
 
-TEST_CASE( "substitute input by constant in NAND-based XOR circuit", "[selected]" )
+TEST_CASE( "substitute input by constant in NAND-based XOR circuit", "[aig]" )
 {
   aig_network aig;
   const auto x1 = aig.create_pi();
@@ -609,4 +609,54 @@ TEST_CASE( "substitute input by constant in NAND-based XOR circuit", "[selected]
   CHECK( aig.fanout_size( aig.get_node( f2 ) ) == 0u );
   CHECK( aig.fanout_size( aig.get_node( f3 ) ) == 0u );
   CHECK( aig.fanout_size( aig.get_node( f4 ) ) == 0u );
+}
+
+TEST_CASE( "substitute node by constant in NAND-based XOR circuit", "[selected]" )
+{
+  aig_network aig;
+  const auto x1 = aig.create_pi();
+  const auto x2 = aig.create_pi();
+
+  const auto f1 = aig.create_nand( x1, x2 );
+  const auto f2 = aig.create_nand( x1, f1 );
+  const auto f3 = aig.create_nand( x2, f1 );
+  const auto f4 = aig.create_nand( f2, f3 );
+  aig.create_po( f4 );
+
+  CHECK( aig.num_gates() == 4u );
+  CHECK( simulate<kitty::static_truth_table<2>>( aig )[0]._bits == 0x6 );
+
+  aig.substitute_node( aig.get_node( f3 ), aig.get_constant( false ) );
+
+  CHECK( simulate<kitty::static_truth_table<2>>( aig )[0]._bits == 0x2 );
+
+  CHECK( aig.fanout_size( aig.get_node( f1 ) ) == 1u );
+  CHECK( aig.fanout_size( aig.get_node( f2 ) ) == 1u );
+  CHECK( aig.fanout_size( aig.get_node( f3 ) ) == 0u );
+  CHECK( aig.fanout_size( aig.get_node( f4 ) ) == 0u );
+}
+
+TEST_CASE( "substitute node by constant in NAND-based XOR circuit (test case 2)", "[selected]" )
+{
+  aig_network aig;
+  const auto x1 = aig.create_pi();
+  const auto x2 = aig.create_pi();
+
+  const auto f1 = aig.create_nand( x1, x2 );
+  const auto f2 = aig.create_nand( x1, f1 );
+  const auto f3 = aig.create_nand( x2, f1 );
+  const auto f4 = aig.create_nand( f2, f3 );
+  aig.create_po( f4 );
+
+  CHECK( aig.num_gates() == 4u );
+  CHECK( simulate<kitty::static_truth_table<2>>( aig )[0]._bits == 0x6 );
+
+  aig.substitute_node( aig.get_node( f1 ), aig.get_constant( false ) );
+
+  CHECK( simulate<kitty::static_truth_table<2>>( aig )[0]._bits == 0xe );
+
+  CHECK( aig.fanout_size( aig.get_node( f1 ) ) == 0u );
+  CHECK( aig.fanout_size( aig.get_node( f2 ) ) == 0u );
+  CHECK( aig.fanout_size( aig.get_node( f3 ) ) == 0u );
+  CHECK( aig.fanout_size( aig.get_node( f4 ) ) == 1u );
 }
