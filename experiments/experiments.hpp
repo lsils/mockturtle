f13e24c0fa9b3379c26da2e1268ebfe5948edd76
Mathias Soeken <mathias.soeken@epfl.ch>
2019-04-03 16:46:38 +0200

    Copyright.

--- experiments/experiments.hpp
+++ experiments/experiments.hpp
@@ -1,3 +1,35 @@
+/* mockturtle: C++ logic network library
+ * Copyright (C) 2018-2019  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*!
+  \file experiments.hpp
+  \brief Framework for simple experimental evaluation
+
+  \author Mathias Soeken
+*/
+
 #include <array>
 #include <cstdio>
 #include <fstream>
@@ -113,7 +145,7 @@ public:
     }
   }
 
-  void save( std::string_view version )
+  void save( std::string_view version = use_github_revision )
   {
     nlohmann::json entries;
     for ( auto const& row : rows_ )
