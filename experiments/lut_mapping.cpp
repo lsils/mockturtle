cccc25417a596639932270a43317b946da6c7302
Mathias Soeken <mathias.soeken@gmail.com>
2019-04-23 16:18:48 +0200

    LUT mapping experiments (#153)
    * No need to write files.
    
    * lut_mapping experiments.

--- experiments/lut_mapping.cpp
+++ experiments/lut_mapping.cpp
@@ -28,13 +28,14 @@
 
 #include <fmt/format.h>
 #include <lorina/aiger.hpp>
-#include <mockturtle/algorithms/cleanup.hpp>
-#include <mockturtle/algorithms/cut_rewriting.hpp>
+#include <mockturtle/algorithms/collapse_mapped.hpp>
 #include <mockturtle/algorithms/equivalence_checking.hpp>
+#include <mockturtle/algorithms/lut_mapping.hpp>
 #include <mockturtle/algorithms/miter.hpp>
-#include <mockturtle/algorithms/node_resynthesis/xag_npn.hpp>
 #include <mockturtle/io/aiger_reader.hpp>
 #include <mockturtle/networks/aig.hpp>
+#include <mockturtle/networks/klut.hpp>
+#include <mockturtle/views/mapping_view.hpp>
 
 #include <experiments.hpp>
 
@@ -43,51 +44,23 @@ int main()
   using namespace experiments;
   using namespace mockturtle;
 
-  std::unordered_map<std::string, double> baseline = {
-    {"adder", 0.00},
-    {"bar", 0.33},
-    {"div", 9.66},
-    {"hyp", 25.70},
-    {"log2", 9.74},
-    {"max", 0.21},
-    {"multiplier", 6.27},
-    {"sin", 1.97},
-    {"sqrt", 5.28},
-    {"square", 2.90},
-    {"arbiter", 0.01},
-    {"cavlc", 0.01},
-    {"ctrl", 0.01},
-    {"dec", 0.00},
-    {"i2c", 0.02},
-    {"int2float", 0.01},
-    {"mem_ctrl", 4.46},
-    {"priority", 0.06},
-    {"router", 0.01},
-    {"voter", 3.54}
-  };
+  experiment<std::string, uint32_t, double, double, bool> exp( "lut_mapping", "benchmark", "luts", "runtime", "runtime cec", "equivalent" );
 
-  experiment<std::string, double, double, bool> exp( "equivalence_checking", "benchmark", "abc cec", "runtime", "equivalent" );
-
-  for ( auto const& benchmark : epfl_benchmarks() )
+  for ( auto const& benchmark : epfl_benchmarks( ( adder | bar | max | experiments::random ) & ~mem_ctrl & ~voter ) )
   {
     fmt::print( "[i] processing {}\n", benchmark );
     aig_network aig;
     lorina::read_aiger( benchmark_path( benchmark ), aiger_reader( aig ) );
-    const auto orig = aig;
 
-    xag_npn_resynthesis<aig_network> resyn;
+    lut_mapping_stats st;
+    mapping_view<aig_network, true> mapped_aig{aig};
+    lut_mapping<decltype( mapped_aig ), true>( mapped_aig, {}, &st );
+    const auto klut = *collapse_mapped_network<klut_network>( mapped_aig );
 
-    cut_rewriting_params ps;
-    ps.cut_enumeration_ps.cut_size = 4;
-    ps.progress = true;
+    equivalence_checking_stats ecst;
+    auto cec = *equivalence_checking( *miter<klut_network>( aig, klut ), {}, &ecst );
 
-    cut_rewriting( aig, resyn, ps );
-    aig = cleanup_dangling( aig );
-
-    equivalence_checking_stats st;
-    auto cec = *equivalence_checking( *miter<aig_network>( orig, aig ), {}, &st );
-
-    exp( benchmark, baseline[benchmark], to_seconds( st.time_total ), cec );
+    exp( benchmark, klut.num_gates(), to_seconds( st.time_total ), to_seconds( ecst.time_total ), cec );
   }
 
   exp.save();
