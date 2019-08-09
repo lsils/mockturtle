3bbbd7bcd10b04da844657698c2cf8fa05c812a2
Mathias Soeken <mathias.soeken@epfl.ch>
2019-08-09 18:11:06 +0200

    Fix bug in DSD decomposition on non-support vars.

--- test/algorithms/dsd_decomposition.cpp
+++ test/algorithms/dsd_decomposition.cpp
@@ -3,6 +3,7 @@
 #include <kitty/constructors.hpp>
 #include <kitty/dynamic_truth_table.hpp>
 #include <mockturtle/algorithms/dsd_decomposition.hpp>
+#include <mockturtle/algorithms/shannon_decomposition.hpp>
 #include <mockturtle/algorithms/simulation.hpp>
 #include <mockturtle/networks/aig.hpp>
 #include <mockturtle/networks/klut.hpp>
@@ -84,4 +85,30 @@ TEST_CASE( "Partial DSD decomposition into k-LUT network", "[dsd_decomposition]"
 
   default_simulator<kitty::dynamic_truth_table> sim( table.num_vars() );
   CHECK( simulate<kitty::dynamic_truth_table>( ntk, sim )[0] == table );
+}
+
+TEST_CASE( "DSD decomposition on random functions of different size", "[dsd_decomposition]" )
+{
+  for ( uint32_t var = 0u; var <= 6u; ++var )
+  {
+    for ( auto i = 0u; i < 100u; ++i )
+    {
+      kitty::dynamic_truth_table func( var );
+      kitty::create_random( func );
+
+      aig_network ntk;
+      std::vector<aig_network::signal> pis( var );
+      std::generate( pis.begin(), pis.end(), [&]() { return ntk.create_pi(); } );
+
+      auto on_prime = [&]( kitty::dynamic_truth_table const& func, std::vector<aig_network::signal> const& pis ) {
+        return shannon_decomposition( ntk, func, pis );
+      };
+
+      ntk.create_po( dsd_decomposition( ntk, func, pis, on_prime ) );
+
+      default_simulator<kitty::dynamic_truth_table> sim( func.num_vars() );
+
+      CHECK( simulate<kitty::dynamic_truth_table>( ntk, sim )[0] == func );
+    }
+  }
 }
