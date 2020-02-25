fe69aa9e1e6a9662c6310e4d3e6792bdd7f245d8
Sonia <lee30sonia@gmail.com>
2020-02-25 12:16:04 +0100

    revert edit in aig_resub

--- experiments/aig_resubstitution.cpp
+++ experiments/aig_resubstitution.cpp
@@ -45,8 +45,6 @@ int main()
 
   for ( auto const& benchmark : epfl_benchmarks() )
   {
-    if ( benchmark == "hyp" || benchmark == "mem_ctrl" || benchmark == "log2" || benchmark == "div" ) continue;
-
     fmt::print( "[i] processing {}\n", benchmark );
     aig_network aig;
     lorina::read_aiger( benchmark_path( benchmark ), aiger_reader( aig ) );
@@ -56,7 +54,6 @@ int main()
 
     ps.max_pis = 8u;
     ps.max_inserts = 1u;
-    ps.verbose = true;
     ps.progress = false;
 
     const uint32_t size_before = aig.num_gates();
@@ -70,7 +67,7 @@ int main()
   }
 
   exp.save();
-  exp.table();
+  exp.compare();
 
   return 0;
 }
