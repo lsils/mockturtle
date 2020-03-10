1abc9b7cac4547d5c8176db8ab3b054932a4aabb
Mathias Soeken <mathias.soeken@epfl.ch>
2020-03-10 18:36:00 +0100

    Some typo in the docs.

--- include/mockturtle/io/index_list.hpp
+++ include/mockturtle/io/index_list.hpp
@@ -62,7 +62,7 @@ namespace mockturtle
  * \return Returns a vector of signals created based on index list
  *
  * Example: The following index list creates (x1 AND x2) XOR (x3 AND x4):
- * `{0x7888, {3 << 16 | 1 << 8 | 4, 2, 4, 6, 8, 12, 10, 14}}`
+ * `{3 << 16 | 1 << 8 | 4, 2, 4, 6, 8, 12, 10, 14}`
  */
 template<class Ntk, class IndexIterator, class LeavesIterator>
 std::vector<signal<Ntk>> create_from_binary_index_list( Ntk& dest, IndexIterator begin, LeavesIterator pi_begin )
