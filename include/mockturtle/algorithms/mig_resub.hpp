6957c8fe89817ed02aee5b27e272b6d90c559822
Mathias Soeken <mathias.soeken@epfl.ch>
2020-03-13 10:11:01 +0100

    Fix warnings. (#269)

--- include/mockturtle/algorithms/mig_resub.hpp
+++ include/mockturtle/algorithms/mig_resub.hpp
@@ -181,7 +181,7 @@ public:
 
   std::optional<signal> operator()( node const& root, TT care, uint32_t required, uint32_t max_inserts, uint32_t num_mffc, uint32_t& last_gain )
   {
-  
+    (void)care;
     assert(is_const0(~care));
 
     /* consider constants */
