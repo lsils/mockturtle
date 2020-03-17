6835795ea14620c0eaa8ffbeb0f503a89c720c0c
Mathias Soeken <mathias.soeken@epfl.ch>
2020-03-17 10:11:57 +0100

    Fix tests and linkage for older GCC.

--- test/algorithms/node_resynthesis/cached.cpp
+++ test/algorithms/node_resynthesis/cached.cpp
@@ -1,11 +1,5 @@
 #include <catch.hpp>
 
-#if __GNUC__ == 7
-#include <experimental/filesystem>
-#else
-#include <filesystem>
-#endif
-
 #include <kitty/constructors.hpp>
 #include <kitty/dynamic_truth_table.hpp>
 
@@ -14,12 +8,6 @@
 #include <mockturtle/networks/xag.hpp>
 
 using namespace mockturtle;
-
-#if __GNUC__ == 7
-namespace fs = std::experimental::filesystem::v1;
-#else
-namespace fs = std::filesystem;
-#endif
 
 TEST_CASE( "Exact XAG for MAJ cached", "[cached]" )
 {
@@ -33,26 +21,20 @@ TEST_CASE( "Exact XAG for MAJ cached", "[cached]" )
 
   std::vector<xag_network::signal> pis = {a, b, c};
 
-  {
-    exact_aig_resynthesis<xag_network> exact_resyn;
-    cached_resynthesis<xag_network, decltype( exact_resyn )> resyn( exact_resyn, 6u, "mockturtle-test-cache.db" );
+  exact_aig_resynthesis<xag_network> exact_resyn;
+  cached_resynthesis<xag_network, decltype( exact_resyn )> resyn( exact_resyn, 6u, "mockturtle-test-cache.db" );
 
-    resyn( xag, maj, pis.begin(), pis.end(), [&]( auto const& f ) {
-      xag.create_po( f );
-    } );
-    resyn( xag, maj, pis.begin(), pis.end(), [&]( auto const& f ) {
-      xag.create_po( f );
-    } );
+  resyn( xag, maj, pis.begin(), pis.end(), [&]( auto const& f ) {
+    xag.create_po( f );
+  } );
+  resyn( xag, maj, pis.begin(), pis.end(), [&]( auto const& f ) {
+    xag.create_po( f );
+  } );
 
-    default_simulator<kitty::dynamic_truth_table> sim( 3u );
-    CHECK( xag.num_pos() == 2u );
-    CHECK( xag.num_gates() == 4u );
-    const auto tts = simulate<kitty::dynamic_truth_table>( xag, sim );
-    CHECK( tts[0] == maj );
-    CHECK( tts[1] == maj );
-  }
-
-  CHECK( fs::exists( "mockturtle-test-cache.db" ) );
-  CHECK( !fs::exists( "mockturtle-test-cache.db.bak" ) );
-  fs::remove( "mockturtle-test-cache.db" );
+  default_simulator<kitty::dynamic_truth_table> sim( 3u );
+  CHECK( xag.num_pos() == 2u );
+  CHECK( xag.num_gates() == 4u );
+  const auto tts = simulate<kitty::dynamic_truth_table>( xag, sim );
+  CHECK( tts[0] == maj );
+  CHECK( tts[1] == maj );
 }
