0a951a178c501ddff12291589f10c557b0eaec52
Mathias Soeken <mathias.soeken@epfl.ch>
2020-03-29 18:32:11 +0200

    Initial version for new rewriting algorithm.

---
+++ include/mockturtle/algorithms/cut_rewriting2.hpp
@@ -0,0 +1,220 @@
+/* mockturtle: C++ logic network library
+ * Copyright (C) 2018-2019  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*!
+  \file cut_rewriting.hpp
+  \brief Cut rewriting
+
+  \author Mathias Soeken
+*/
+
+#pragma once
+
+#include <cstdint>
+
+#include "../traits.hpp"
+#include "../utils/node_map.hpp"
+#include "../utils/stopwatch.hpp"
+#include "cleanup.hpp"
+#include "cut_enumeration.hpp"
+#include "detail/mffc_utils.hpp"
+
+#include <fmt/format.h>
+#include <kitty/dynamic_truth_table.hpp>
+
+namespace mockturtle::future
+{
+
+struct cut_rewriting_params
+{
+  /*! \brief Cut enumeration parameters. */
+  cut_enumeration_params cut_enumeration_ps;
+
+  /*! \brief Allow zero-gain substitutions. */
+  bool allow_zero_gain{false};
+
+  /*! \brief Be verbose. */
+  bool verbose{false};
+};
+
+struct cut_rewriting_stats
+{
+  /*! \brief Total runtime. */
+  stopwatch<>::duration time_total{};
+
+  /*! \brief Cut enumeration runtime. */
+  stopwatch<>::duration time_cuts{};
+
+  void report()
+  {
+    fmt::print( "[i] total time     = {:>5.2f} secs\n", to_seconds( time_total ) );
+    fmt::print( "[i] cut enum. time = {:>5.2f} secs\n", to_seconds( time_cuts ) );
+  }
+};
+
+namespace detail
+{
+
+using namespace mockturtle::detail;
+
+template<class Ntk, class RewritingFn>
+struct cut_rewriting_impl
+{
+  cut_rewriting_impl( Ntk const& ntk, RewritingFn&& rewriting_fn, cut_rewriting_params const& ps, cut_rewriting_stats& st )
+      : ntk_( ntk ),
+        rewriting_fn_( rewriting_fn ),
+        ps_( ps ),
+        st_( st ) {}
+
+  Ntk run()
+  {
+    stopwatch t( st_.time_total );
+
+    /* initial node map */
+    Ntk res;
+    node_map<signal<Ntk>, Ntk> old2new( ntk_ );
+    old2new[ntk_.get_constant( false )] = res.get_constant( false );
+    if ( ntk_.get_node( ntk_.get_constant( true ) ) != ntk_.get_node( ntk_.get_constant( false ) ) )
+    {
+      old2new[ntk_.get_constant( true )] = res.get_constant( true );
+    }
+    ntk_.foreach_pi( [&]( auto const& n ) {
+      old2new[n] = res.create_pi();
+    } );
+
+    /* enumerate cuts */
+    const auto cuts = call_with_stopwatch( st_.time_cuts, [&]() { return cut_enumeration<Ntk, true, cut_enumeration_cut_rewriting_cut>( ntk_, ps_.cut_enumeration_ps ); } );
+
+    /* for cost estimation we use reference counters initialized by the fanout size */
+    ntk_.clear_values();
+    ntk_.foreach_node( [&]( auto const& n ) {
+      ntk_.set_value( n, ntk_.fanout_size( n ) );
+    } );
+    res.events().on_add.push_back( [&]( auto const& n ) {
+      res.foreach_fanin( n, [&]( auto const& f ) {
+        res.incr_value( res.get_node( f ) );
+      } );
+    } );
+
+    ntk_.foreach_gate( [&]( auto const& n ) {
+      // TODO progress bar
+
+      /* nothing to optimize? */
+      int32_t value = mffc_size( ntk_, n );
+      if ( value == 1 )
+      {
+        std::vector<signal<Ntk>> children( ntk_.fanin_size( n ) );
+        ntk_.foreach_fanin( n, [&]( auto const& f, auto i ) {
+          children[i] = old2new[f] ^ ntk_.is_complemented( f );
+        } );
+
+        old2new[n] = res.clone_node( ntk_, n, children );
+        return;
+      }
+
+      /* foreach cut */
+      int32_t best_gain = -1;
+      signal<Ntk> best_signal;
+      for ( auto& cut : cuts.cuts( ntk_.node_to_index( n ) ) )
+      {
+        // TOOO skip cuts
+        if ( cut->size() <= 2 ) continue;
+
+        const auto tt = cuts.truth_table( *cut );
+        assert( cut->size() == static_cast<unsigned>( tt.num_vars() ) );
+
+        // TODO progress bar
+
+        std::vector<signal<Ntk>> children( cut->size() );
+        auto ctr = 0u;
+        for ( auto l : *cut )
+        {
+          children[ctr++] = old2new[ntk_.index_to_node( l )];
+        }
+
+        const auto on_signal = [&]( auto const& f_new ) {
+          int32_t gain = value - mffc_size( res, res.get_node( f_new ) );
+
+          if ( ( gain > 0 || ( ps_.allow_zero_gain && gain == 0 ) ) && gain > best_gain )
+          {
+            best_gain = gain;
+            best_signal = f_new;
+          }
+
+          return true;
+        };
+        rewriting_fn_( res, cuts.truth_table( *cut ), children.begin(), children.end(), on_signal );
+      }
+
+      if ( best_gain == -1 )
+      {
+        std::vector<signal<Ntk>> children( ntk_.fanin_size( n ) );
+        ntk_.foreach_fanin( n, [&]( auto const& f, auto i ) {
+          children[i] = old2new[f] ^ ntk_.is_complemented( f );
+        } );
+
+        old2new[n] = res.clone_node( ntk_, n, children );
+      }
+      else
+      {
+        old2new[n] = best_signal;
+      }
+    } );
+
+    /* create POs */
+    ntk_.foreach_po( [&]( auto const& f ) {
+      res.create_po( old2new[f] ^ ntk_.is_complemented( f ) );
+    } );
+
+    return cleanup_dangling( res );
+  }
+
+private:
+  Ntk const& ntk_;
+  RewritingFn&& rewriting_fn_;
+  cut_rewriting_params const& ps_;
+  cut_rewriting_stats& st_;
+};
+
+} // namespace detail
+
+template<class Ntk, class RewritingFn>
+Ntk cut_rewriting( Ntk const& ntk, RewritingFn&& rewriting_fn, cut_rewriting_params const& ps = {}, cut_rewriting_stats* pst = nullptr )
+{
+  cut_rewriting_stats st;
+  const auto result = detail::cut_rewriting_impl<Ntk, RewritingFn>( ntk, rewriting_fn, ps, st ).run();
+
+  if ( ps.verbose )
+  {
+    st.report();
+  }
+  if ( pst )
+  {
+    *pst = st;
+  }
+  return result;
+}
+
+} // namespace mockturtle::future
