66b6eff25a1387271cc139b54c8deec660c2de50
Heinz Riener <heinz.riener@gmail.com>
2020-09-30 15:51:40 +0200

    window_view.

--- include/mockturtle/views/window_view.hpp
+++ include/mockturtle/views/window_view.hpp
@@ -32,21 +32,244 @@
 
 #pragma once
 
-#include <algorithm>
-#include <cstdint>
-#include <unordered_map>
-#include <vector>
-#include <set>
-#include <cassert>
-
 #include "../traits.hpp"
 #include "../networks/detail/foreach.hpp"
 #include "immutable_view.hpp"
 
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <set>
+#include <unordered_map>
+#include <vector>
+
 namespace mockturtle
 {
 
-/*! \brief Implements an isolated view on a window in a network. */
+/*! \brief Implements an isolated view on a window in a network.
+ *
+ * This view creates a network from a window in a large network.  The
+ * window is specified by three parameters:
+ *   1.) `inputs` are the common support of all window nodes, they do
+ *       not overlap with `gates` (i.e., the intersection of `inputs` and
+ *       `gates` is the empty set).
+ *   2.) `gates` are the nodes in the window, supported by the
+ *       `inputs` (i.e., `gates` are in the transitive fanout of the
+ *       `inputs`,
+ *   3.) `outputs` are signals (regular or complemented nodes)
+ *        pointing to nodes in `gates` or `inputs`.  Not all fanouts
+ *        of an output node are already part of the window.
+ *
+ * The second parameter `gates` has to be passed and is not
+ * automatically computed (for example in contrast to `cut_view`),
+ * because there are different strategies to construct a window from a
+ * support set.  The outputs could be automatically computed.
+ *
+ * The window_view implements one new API method:
+ *   1.) `belongs_to_window`: takes a node as input and returns true
+ *       if and only if this node is a constant, an input, or an inner
+ *       node of the window
+ */
+template<typename Ntk>
+class new_window_view : public immutable_view<Ntk>
+{
+public:
+  using storage = typename Ntk::storage;
+  using node = typename Ntk::node;
+  using signal = typename Ntk::signal;
+
+public:
+  explicit new_window_view( Ntk const& ntk, std::vector<node> const& inputs, std::vector<signal> const& outputs, std::vector<node> const& gates )
+    : immutable_view<Ntk>( ntk )
+    , _inputs( inputs )
+    , _outputs( outputs )
+  {
+    construct( inputs, gates );
+  }
+
+  explicit new_window_view( Ntk const& ntk, std::vector<node> const& inputs, std::vector<node> const& outputs, std::vector<node> const& gates )
+    : immutable_view<Ntk>( ntk )
+    , _inputs( inputs )
+  {
+    construct( inputs, gates );
+
+    /* convert output nodes to signals */
+    std::transform( std::begin( outputs ), std::end( outputs ), std::back_inserter( _outputs ),
+                    [this]( node const& n ){
+                      return this->make_signal( n );
+                    });
+  }
+
+#pragma region Window
+  inline bool belongs_to_window( node const& n ) const
+  {
+    return std::find( std::begin( _nodes ), std::end( _nodes ), n ) != std::end( _nodes );
+  }
+#pragma endregion
+
+#pragma region Structural properties
+  inline uint32_t size() const
+  {
+    return static_cast<uint32_t>( _nodes.size() );
+  }
+
+  inline uint32_t num_cis() const
+  {
+    return num_pis();
+  }
+
+  inline uint32_t num_cos() const
+  {
+    return num_pos();
+  }
+
+  inline uint32_t num_latches() const
+  {
+    return 0u;
+  }
+
+  inline uint32_t num_pis() const
+  {
+    return static_cast<uint32_t>( _inputs.size() );
+  }
+
+  inline uint32_t num_pos() const
+  {
+    return static_cast<uint32_t>( _outputs.size() );
+  }
+
+  inline uint32_t num_registers() const
+  {
+    return 0u;
+  }
+
+  inline uint32_t num_gates() const
+  {
+    return static_cast<uint32_t>( _nodes.size() - _inputs.size() - 1u );
+  }
+
+  inline uint32_t node_to_index( node const& n ) const
+  {
+    return _node_to_index.at( n );
+  }
+
+  inline node index_to_node( uint32_t index ) const
+  {
+    return _nodes[index];
+  }
+
+  inline bool is_pi( node const& n ) const
+  {
+    return std::find( std::begin( _inputs ), std::end( _inputs ), n ) != std::end( _inputs );
+  }
+
+  inline bool is_ci( node const& n ) const
+  {
+    return is_pi( n );
+  }
+
+#pragma endregion
+
+#pragma region Node and signal iterators
+  template<typename Fn>
+  void foreach_pi( Fn&& fn ) const
+  {
+    detail::foreach_element( std::begin( _inputs ), std::end( _inputs ), fn );
+  }
+
+  template<typename Fn>
+  void foreach_po( Fn&& fn ) const
+  {
+    detail::foreach_element( std::begin( _outputs ), std::end( _outputs ), fn );
+  }
+
+  template<typename Fn>
+  void foreach_ci( Fn&& fn ) const
+  {
+    foreach_pi( fn );
+  }
+
+  template<typename Fn>
+  void foreach_co( Fn&& fn ) const
+  {
+    foreach_po( fn );
+  }
+
+  template<typename Fn>
+  void foreach_ro( Fn&& fn ) const
+  {
+    (void)fn;
+  }
+
+  template<typename Fn>
+  void foreach_ri( Fn&& fn ) const
+  {
+    (void)fn;
+  }
+
+  template<typename Fn>
+  void foreach_register( Fn&& fn ) const
+  {
+    (void)fn;
+  }
+
+  template<typename Fn>
+  void foreach_node( Fn&& fn ) const
+  {
+    detail::foreach_element( std::begin( _nodes ), std::end( _nodes ), fn );
+  }
+
+  template<typename Fn>
+  void foreach_gate( Fn&& fn ) const
+  {
+    detail::foreach_element( std::begin( _nodes ) + 1u + _inputs.size(), std::end( _nodes ), fn );
+  }
+
+  template<typename Fn>
+  void foreach_fanin( node const& n, Fn&& fn ) const
+  {
+    /* constants and inputs do not have fanins */
+    if ( this->is_constant( n ) ||
+         std::find( std::begin( _inputs ), std::end( _inputs ), n ) != std::end( _inputs ) )
+    {
+      return;
+    }
+
+    /* if it's not a window input, the node has to be a window node */
+    assert( std::find( std::begin( _nodes ) + 1 + _inputs.size(), std::end( _nodes ), n ) != std::end( _nodes ) );
+    immutable_view<Ntk>::foreach_fanin( n, fn );
+  }
+#pragma endregion
+
+protected:
+  void construct( std::vector<node> const& inputs, std::vector<node> const& gates )
+  {
+    /* copy constant to nodes */
+    _nodes.emplace_back( this->get_node( this->get_constant( false ) ) );
+
+    /* copy inputs to nodes */
+    std::copy( std::begin( inputs ), std::end( inputs ), std::back_inserter( _nodes ) );
+
+    /* copy gates to nodes */
+    std::copy( std::begin( gates ), std::end( gates ), std::back_inserter( _nodes ) );
+
+    /* create a mapping from node id (index in the original network) to window index */
+    for ( uint32_t index = 0; index < _nodes.size(); ++index )
+    {
+      _node_to_index[_nodes.at( index )] = index;
+    }
+  }
+
+protected:
+  std::vector<node> _inputs;
+  std::vector<signal> _outputs;
+  std::vector<node> _nodes;
+  std::unordered_map<node, uint32_t> _node_to_index;
+}; /* new_window_view */
+
+/*! \brief Implements an isolated view on a window in a network.
+ *
+ */
 template<typename Ntk>
 class window_view : public immutable_view<Ntk>
 {
--- test/views/window_view.cpp
+++ test/views/window_view.cpp
@@ -8,6 +8,193 @@
 
 using namespace mockturtle;
 
+template<typename Ntk>
+std::vector<typename Ntk::node> collect_fanin_nodes( Ntk const& ntk, typename Ntk::node const& n )
+{
+  using node = typename Ntk::node;
+  using signal = typename Ntk::signal;
+
+  std::vector<node> fanin_nodes;
+  ntk.foreach_fanin( n, [&]( signal const& fi ){
+    fanin_nodes.emplace_back( ntk.get_node( fi ) );
+  });
+  return fanin_nodes;
+}
+
+/*! \brief Ensure that all outputs and fanins belong to the window */
+template<typename Ntk>
+bool window_is_well_formed( Ntk const& ntk )
+{
+  using node = typename Ntk::node;
+  using signal = typename Ntk::signal;
+
+  bool all_fanins_belong_to_window = true;
+  ntk.foreach_node( [&]( node const& n ){
+    ntk.foreach_fanin( n, [&]( signal const& fi ){
+      if ( !ntk.belongs_to_window( ntk.get_node( fi ) ) )
+      {
+        all_fanins_belong_to_window = false;
+        return false; /* terminate */
+      }
+      return true; /* next */
+    });
+
+    /* check if property is already violated */
+    if ( !all_fanins_belong_to_window )
+    {
+      return false; /* terminate */
+    }
+    return true; /* next */
+  });
+
+  bool all_outputs_belong_to_window = true;
+  ntk.foreach_po( [&]( signal const& o ){
+    if ( !ntk.belongs_to_window( ntk.get_node( o ) ) )
+    {
+      all_outputs_belong_to_window = false;
+      return false; /* terminate */
+    }
+    return true; /* next */
+  });
+
+  return all_fanins_belong_to_window &&
+         all_outputs_belong_to_window;
+}
+
+TEST_CASE( "create new window view on AIG", "[window_view]" )
+{
+  aig_network aig;
+  const auto a = aig.create_pi();
+  const auto b = aig.create_pi();
+  const auto f1 = aig.create_nand( a, b );
+  const auto f2 = aig.create_nand( a, f1 );
+  const auto f3 = aig.create_nand( b, f1 );
+  const auto f4 = aig.create_nand( f2, f3 );
+  aig.create_po( f4 );
+
+  CHECK( aig.size() == 7 );
+  CHECK( aig.num_pis() == 2 );
+  CHECK( aig.num_pos() == 1 );
+  CHECK( aig.num_gates() == 4 );
+
+  /* make a window */
+  {
+    new_window_view view( aig,
+                          /* inputs = */ { aig.get_node( a ), aig.get_node( b ) },
+                          /* outputs = */ { f3 },
+                          /* nodes = */ { aig.get_node( f1 ), aig.get_node( f3 ) } );
+    CHECK( view.size() == 5 );
+    CHECK( view.num_gates() == 2 );
+    CHECK( view.num_pis() == 2 );
+    CHECK( view.num_pos() == 1 );
+    CHECK( view.num_cis() == 2 );
+    CHECK( view.num_cos() == 1 );
+
+    CHECK(  view.belongs_to_window( view.get_node( f1 ) ) );
+    CHECK( !view.belongs_to_window( view.get_node( f2 ) ) );
+    CHECK(  view.belongs_to_window( view.get_node( f3 ) ) );
+    CHECK( !view.belongs_to_window( view.get_node( f4 ) ) );
+
+    CHECK( collect_fanin_nodes( view, view.get_node( f1 ) ).size() == 2 );
+    CHECK( collect_fanin_nodes( view, view.get_node( f3 ) ).size() == 2 );
+    CHECK( window_is_well_formed( view ) );
+  }
+
+  {
+    new_window_view view( aig,
+                          /* inputs = */ { aig.get_node( f1 ), aig.get_node( b ) },
+                          /* outputs = */ { f3 },
+                          /* nodes = */ { aig.get_node( f3 ) } );
+    CHECK( view.size() == 4 );
+    CHECK( view.num_gates() == 1 );
+    CHECK( view.num_pis() == 2 );
+    CHECK( view.num_pos() == 1 );
+    CHECK( view.num_cis() == 2 );
+    CHECK( view.num_cos() == 1 );
+
+    CHECK(  view.belongs_to_window( view.get_node( f1 ) ) );
+    CHECK( !view.belongs_to_window( view.get_node( f2 ) ) );
+    CHECK(  view.belongs_to_window( view.get_node( f3 ) ) );
+    CHECK( !view.belongs_to_window( view.get_node( f4 ) ) );
+
+    CHECK( collect_fanin_nodes( view, view.get_node( f1 ) ).size() == 0 );
+    CHECK( collect_fanin_nodes( view, view.get_node( f3 ) ).size() == 2 );
+    CHECK( window_is_well_formed( view ) );
+  }
+
+  {
+    new_window_view view( aig,
+                          /* inputs = */ { aig.get_node( a ), aig.get_node( b ) },
+                          /* outputs = */ { f2 },
+                          /* nodes = */ { aig.get_node( f1 ), aig.get_node( f2 ) } );
+
+    CHECK( view.size() == 5 );
+    CHECK( view.num_gates() == 2 );
+    CHECK( view.num_pis() == 2 );
+    CHECK( view.num_pos() == 1 );
+    CHECK( view.num_cis() == 2 );
+    CHECK( view.num_cos() == 1 );
+
+    CHECK(  view.belongs_to_window( view.get_node( f1 ) ) );
+    CHECK(  view.belongs_to_window( view.get_node( f2 ) ) );
+    CHECK( !view.belongs_to_window( view.get_node( f3 ) ) );
+    CHECK( !view.belongs_to_window( view.get_node( f4 ) ) );
+
+    CHECK( collect_fanin_nodes( view, view.get_node( f1 ) ).size() == 2 );
+    CHECK( collect_fanin_nodes( view, view.get_node( f2 ) ).size() == 2 );
+    CHECK( window_is_well_formed( view ) );
+  }
+
+  {
+    new_window_view view( aig,
+                          /* inputs = */ { aig.get_node( a ), aig.get_node( b ) },
+                          /* outputs = */ { f4 },
+                          /* nodes = */ { aig.get_node( f1 ), aig.get_node( f2 ), aig.get_node( f3 ), aig.get_node( f4 ) } );
+
+    CHECK( view.size() == 7 );
+    CHECK( view.num_gates() == 4 );
+    CHECK( view.num_pis() == 2 );
+    CHECK( view.num_pos() == 1 );
+    CHECK( view.num_cis() == 2 );
+    CHECK( view.num_cos() == 1 );
+
+    CHECK( view.belongs_to_window( view.get_node( f1 ) ) );
+    CHECK( view.belongs_to_window( view.get_node( f2 ) ) );
+    CHECK( view.belongs_to_window( view.get_node( f3 ) ) );
+    CHECK( view.belongs_to_window( view.get_node( f4 ) ) );
+
+    CHECK( collect_fanin_nodes( view, view.get_node( f1 ) ).size() == 2 );
+    CHECK( collect_fanin_nodes( view, view.get_node( f2 ) ).size() == 2 );
+    CHECK( collect_fanin_nodes( view, view.get_node( f3 ) ).size() == 2 );
+    CHECK( collect_fanin_nodes( view, view.get_node( f4 ) ).size() == 2 );
+    CHECK( window_is_well_formed( view ) );
+  }
+
+  {
+    new_window_view view( aig,
+                          /* inputs = */ { aig.get_node( a ), aig.get_node( b ) },
+                          /* outputs = */ { f2, f3 },
+                          /* nodes = */ { aig.get_node( f1 ), aig.get_node( f2 ), aig.get_node( f3 ) } );
+
+    CHECK( view.size() == 6 );
+    CHECK( view.num_gates() == 3 );
+    CHECK( view.num_pis() == 2 );
+    CHECK( view.num_pos() == 2 );
+    CHECK( view.num_cis() == 2 );
+    CHECK( view.num_cos() == 2 );
+
+    CHECK(  view.belongs_to_window( view.get_node( f1 ) ) );
+    CHECK(  view.belongs_to_window( view.get_node( f2 ) ) );
+    CHECK(  view.belongs_to_window( view.get_node( f3 ) ) );
+    CHECK( !view.belongs_to_window( view.get_node( f4 ) ) );
+
+    CHECK( collect_fanin_nodes( view, view.get_node( f1 ) ).size() == 2 );
+    CHECK( collect_fanin_nodes( view, view.get_node( f2 ) ).size() == 2 );
+    CHECK( collect_fanin_nodes( view, view.get_node( f3 ) ).size() == 2 );
+    CHECK( window_is_well_formed( view ) );
+  }
+}
+
 TEST_CASE( "create window view on AIG", "[window_view]" )
 {
   aig_network aig;
