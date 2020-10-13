a93f4cf4071695850245796d38079b755c9f7e61
Heinz Riener <heinz.riener@gmail.com>
2020-10-13 20:31:23 +0200

    index_list: more documentation and checks.

--- include/mockturtle/utils/index_list.hpp
+++ include/mockturtle/utils/index_list.hpp
@@ -29,7 +29,10 @@
   \author Heinz Riener
 */
 
+#include "../traits.hpp"
+
 #include <fmt/format.h>
+
 #include <vector>
 #include <array>
 
@@ -41,6 +44,10 @@ namespace mockturtle
  * Small network represented as a list of literals.  The
  * implementation supports AND and XOR gates and is compatiable with
  * ABC's encoding.
+ *
+ * Example: The following index list creates the output function `(x1
+ * AND x2) XOR (x3 AND x4)` with 4 inputs, 1 output, and 3 gates:
+ * `{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8, 12, 10, 14, 14}`
  */
 struct abc_index_list
 {
@@ -167,8 +174,8 @@ private:
  * - `foreach_fanin`
  * - `foreach_gate`
  * - `get_node`
- * - `is_complemented`
  * - `is_and`
+ * - `is_complemented`
  * - `is_xor`
  * - `node_to_index`
  * - `num_gates`
@@ -181,18 +188,48 @@ private:
 template<typename Ntk>
 void encode( abc_index_list& indices, Ntk const& ntk )
 {
+  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
+  static_assert( has_foreach_fanin_v<Ntk>, "Ntk does not implement the foreach_fanin method" );
+  static_assert( has_foreach_gate_v<Ntk>, "Ntk does not implement the foreach_gate method" );
+  static_assert( has_is_and_v<Ntk>, "Ntk does not implement the is_and method" );
+  static_assert( has_get_node_v<Ntk>, "Ntk does not implement the get_node method" );
+  static_assert( has_is_complemented_v<Ntk>, "Ntk does not implement the is_complemented method" );
+  static_assert( has_is_xor_v<Ntk>, "Ntk does not implement the is_xor method" );
+  static_assert( has_node_to_index_v<Ntk>, "Ntk does not implement the node_to_index method" );
+  static_assert( has_num_gates_v<Ntk>, "Ntk does not implement the num_gates method" );
+  static_assert( has_num_pis_v<Ntk>, "Ntk does not implement the num_pis method" );
+  static_assert( has_num_pos_v<Ntk>, "Ntk does not implement the num_pos method" );
+
   using node   = typename Ntk::node;
   using signal = typename Ntk::signal;
+
+  ntk.foreach_pi( [&]( node const& n, uint64_t i ) {
+    if ( ntk.node_to_index( n ) != i + 1 )
+    {
+      fmt::print( "[e] network is not in normalized index order (violated by PI {})\n", i + 1 );
+      std::abort();
+    }
+  });
 
   /* inputs */
   indices.add_inputs( ntk.num_pis() );
 
   /* gates */
-  ntk.foreach_gate( [&]( node const& n ){
+  ntk.foreach_gate( [&]( node const& n, uint64_t index ){
     assert( ntk.is_and( n ) || ntk.is_xor( n ) );
+    if ( ntk.node_to_index( n ) != ntk.num_pis() + index + 1 )
+    {
+      fmt::print( "[e] network is not in normalized index order (violated by node {})\n", ntk.node_to_index( n ) );
+      std::abort();
+    }
 
     std::array<uint32_t, 2u> lits;
-    ntk.foreach_fanin( n, [&]( signal const& fi, uint32_t index ){
+    ntk.foreach_fanin( n, [&]( signal const& fi, uint64_t index ){
+      if ( ntk.node_to_index( ntk.get_node( fi ) ) > ntk.node_to_index( n ) )
+      {
+        fmt::print( "[e] node {} not in topological order\n", ntk.node_to_index( n ) );
+        std::abort();
+      }
       lits[index] = 2*ntk.node_to_index( ntk.get_node( fi ) ) + ntk.is_complemented( fi );
     });
 
@@ -230,6 +267,14 @@ void encode( abc_index_list& indices, Ntk const& ntk )
 template<typename Ntk, typename BeginIter, typename EndIter, typename Fn>
 void insert( Ntk& ntk, BeginIter begin, EndIter end, abc_index_list const& indices, Fn&& fn )
 {
+  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
+  static_assert( has_create_and_v<Ntk>, "Ntk does not implement the create_and method" );
+  static_assert( has_create_xor_v<Ntk>, "Ntk does not implement the create_xor method" );
+  static_assert( has_get_constant_v<Ntk>, "Ntk does not implement the get_constant method" );
+
+  static_assert( std::is_same_v<std::decay_t<typename std::iterator_traits<BeginIter>::value_type>, signal<Ntk>>, "BeginIter value_type must be Ntk signal type" );
+  static_assert( std::is_same_v<std::decay_t<typename std::iterator_traits<EndIter>::value_type>, signal<Ntk>>, "EndIter value_type must be Ntk signal type" );
+
   using signal = typename Ntk::signal;
 
   std::vector<signal> signals;
@@ -247,14 +292,7 @@ void insert( Ntk& ntk, BeginIter begin, EndIter end, abc_index_list const& indic
     signal const s0 = ( lit0 % 2 ) ? !signals.at( i0 ) : signals.at( i0 );
     signal const s1 = ( lit1 % 2 ) ? !signals.at( i1 ) : signals.at( i1 );
 
-    if ( lit0 < lit1 )
-    {
-      signals.push_back( ntk.create_and( s0, s1 ) );
-    }
-    else if ( lit0 > lit1 )
-    {
-      signals.push_back( ntk.create_xor( s0, s1 ) );
-    }
+    signals.push_back( lit0 < lit1 ? ntk.create_and( s0, s1 ) : ntk.create_xor( s0, s1 ) );
   });
 
   indices.foreach_po( [&]( uint32_t lit ){
@@ -267,6 +305,10 @@ void insert( Ntk& ntk, BeginIter begin, EndIter end, abc_index_list const& indic
  *
  * Small network consisting of majority gates and inverters
  * represented as a list of literals.
+ *
+ * Example: The following index list creates the output function
+ * `<<x1, x2, x3>, x2, x4>` with 4 inputs, 1 output, and 3 gates:
+ * `{4 | 1 << 8 | 2 << 16, 2, 4, 6, 4, 8, 10, 12}`
  */
 struct mig_index_list
 {
@@ -374,18 +416,47 @@ private:
 template<typename Ntk>
 void encode( mig_index_list& indices, Ntk const& ntk )
 {
+  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
+  static_assert( has_foreach_fanin_v<Ntk>, "Ntk does not implement the foreach_fanin method" );
+  static_assert( has_foreach_gate_v<Ntk>, "Ntk does not implement the foreach_gate method" );
+  static_assert( has_get_node_v<Ntk>, "Ntk does not implement the get_node method" );
+  static_assert( has_is_complemented_v<Ntk>, "Ntk does not implement the is_complemented method" );
+  static_assert( has_is_maj_v<Ntk>, "Ntk does not implement the is_maj method" );
+  static_assert( has_node_to_index_v<Ntk>, "Ntk does not implement the node_to_index method" );
+  static_assert( has_num_gates_v<Ntk>, "Ntk does not implement the num_gates method" );
+  static_assert( has_num_pis_v<Ntk>, "Ntk does not implement the num_pis method" );
+  static_assert( has_num_pos_v<Ntk>, "Ntk does not implement the num_pos method" );
+
   using node   = typename Ntk::node;
   using signal = typename Ntk::signal;
+
+  ntk.foreach_pi( [&]( node const& n, uint64_t index ) {
+    if ( ntk.node_to_index( n ) != index + 1 )
+    {
+      fmt::print( "[e] network is not in normalized index order (violated by PI {})\n", index + 1 );
+      std::abort();
+    }
+  });
 
   /* inputs */
   indices.add_inputs( ntk.num_pis() );
 
   /* gates */
-  ntk.foreach_gate( [&]( node const& n ){
+  ntk.foreach_gate( [&]( node const& n, uint64_t index ){
     assert( ntk.is_maj( n ) );
+    if ( ntk.node_to_index( n ) != ntk.num_pis() + index + 1 )
+    {
+      fmt::print( "[e] network is not in normalized index order (violated by node {})\n", ntk.node_to_index( n ) );
+      std::abort();
+    }
 
     std::array<uint32_t, 3u> lits;
     ntk.foreach_fanin( n, [&]( signal const& fi, uint64_t index ){
+      if ( ntk.node_to_index( ntk.get_node( fi ) ) > ntk.node_to_index( n ) )
+      {
+        fmt::print( "[e] node {} not in topological order\n", ntk.node_to_index( n ) );
+        std::abort();
+      }
       lits[index] = 2*ntk.node_to_index( ntk.get_node( fi ) ) + ntk.is_complemented( fi );
     });
     indices.add_maj( lits[0u], lits[1u], lits[2u] );
@@ -414,6 +485,13 @@ void encode( mig_index_list& indices, Ntk const& ntk )
 template<typename Ntk, typename BeginIter, typename EndIter, typename Fn>
 void insert( Ntk& ntk, BeginIter begin, EndIter end, mig_index_list const& indices, Fn&& fn )
 {
+  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
+  static_assert( has_create_maj_v<Ntk>, "Ntk does not implement the create_maj method" );
+  static_assert( has_get_constant_v<Ntk>, "Ntk does not implement the get_constant method" );
+
+  static_assert( std::is_same_v<std::decay_t<typename std::iterator_traits<BeginIter>::value_type>, signal<Ntk>>, "BeginIter value_type must be Ntk signal type" );
+  static_assert( std::is_same_v<std::decay_t<typename std::iterator_traits<EndIter>::value_type>, signal<Ntk>>, "EndIter value_type must be Ntk signal type" );
+
   using signal = typename Ntk::signal;
 
   std::vector<signal> signals;
@@ -442,7 +520,22 @@ void insert( Ntk& ntk, BeginIter begin, EndIter end, mig_index_list const& indic
 /*! \brief Index list for xor-and graphs.
  *
  * Small network represented as a list of literals. Supports XOR and
- * AND gates.
+ * AND gates.  The list has the following 32-bit unsigned integer
+ * elements.  It starts with a signature whose partitioned into `|
+ * num_gates | num_pos | num_pis |`, where `num_gates` accounts for
+ * the most-significant 16 bits, `num_pos` accounts for 8 bits, and
+ * `num_pis` accounts for the least-significant 8 bits.  Afterwards,
+ * gates are defined as literal indexes `(2 * i + c)`, where `i` is an
+ * index, with 0 indexing the constant 0, 1 to `num_pis` indexing the
+ * primary inputs, and all successive indexes for the gates.  Gate
+ * literals come in pairs.  If the first literal has a smaller value
+ * than the second one, an AND gate is created, otherwise, an XOR gate
+ * is created.  Afterwards, all outputs are defined in terms of
+ * literals.
+ *
+ * Example: The following index list creates the output function `(x1
+ * AND x2) XOR (x3 AND x4)` with 4 inputs, 1 output, and 3 gates:
+ * `{4 | 1 << 8 | 3 << 16, 2, 4, 6, 8, 12, 10, 14}`
  */
 struct xag_index_list
 {
@@ -558,18 +651,48 @@ private:
 template<typename Ntk>
 void encode( xag_index_list& indices, Ntk const& ntk )
 {
+  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
+  static_assert( has_foreach_fanin_v<Ntk>, "Ntk does not implement the foreach_fanin method" );
+  static_assert( has_foreach_gate_v<Ntk>, "Ntk does not implement the foreach_gate method" );
+  static_assert( has_is_and_v<Ntk>, "Ntk does not implement the is_and method" );
+  static_assert( has_get_node_v<Ntk>, "Ntk does not implement the get_node method" );
+  static_assert( has_is_complemented_v<Ntk>, "Ntk does not implement the is_complemented method" );
+  static_assert( has_is_xor_v<Ntk>, "Ntk does not implement the is_xor method" );
+  static_assert( has_node_to_index_v<Ntk>, "Ntk does not implement the node_to_index method" );
+  static_assert( has_num_gates_v<Ntk>, "Ntk does not implement the num_gates method" );
+  static_assert( has_num_pis_v<Ntk>, "Ntk does not implement the num_pis method" );
+  static_assert( has_num_pos_v<Ntk>, "Ntk does not implement the num_pos method" );
+
   using node   = typename Ntk::node;
   using signal = typename Ntk::signal;
+
+  ntk.foreach_pi( [&]( node const& n, uint64_t index ) {
+    if ( ntk.node_to_index( n ) != index + 1 )
+    {
+      fmt::print( "[e] network is not in normalized index order (violated by PI {})\n", index + 1 );
+      std::abort();
+    }
+  });
 
   /* inputs */
   indices.add_inputs( ntk.num_pis() );
 
   /* gates */
-  ntk.foreach_gate( [&]( node const& n ){
+  ntk.foreach_gate( [&]( node const& n, uint64_t index ){
     assert( ntk.is_and( n ) || ntk.is_xor( n ) );
+    if ( ntk.node_to_index( n ) != ntk.num_pis() + index + 1 )
+    {
+      fmt::print( "[e] network is not in normalized index order (violated by node {})\n", ntk.node_to_index( n ) );
+      std::abort();
+    }
 
     std::array<uint32_t, 2u> lits;
     ntk.foreach_fanin( n, [&]( signal const& fi, uint64_t index ){
+      if ( ntk.node_to_index( ntk.get_node( fi ) ) > ntk.node_to_index( n ) )
+      {
+        fmt::print( "[e] node {} not in topological order\n", ntk.node_to_index( n ) );
+        std::abort();
+      }
       lits[index] = 2*ntk.node_to_index( ntk.get_node( fi ) ) + ntk.is_complemented( fi );
     });
 
@@ -594,8 +717,9 @@ void encode( xag_index_list& indices, Ntk const& ntk )
 /*! \brief Inserts a xag_index_list into an existing network
  *
  * **Required network functions:**
+ * - `create_and`
+ * - `create_xor`
  * - `get_constant`
- * - `create_maj`
  *
  * \param ntk A logic network
  * \param begin Begin iterator of signal inputs
@@ -606,6 +730,14 @@ void encode( xag_index_list& indices, Ntk const& ntk )
 template<typename Ntk, typename BeginIter, typename EndIter, typename Fn>
 void insert( Ntk& ntk, BeginIter begin, EndIter end, xag_index_list const& indices, Fn&& fn )
 {
+  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
+  static_assert( has_create_and_v<Ntk>, "Ntk does not implement the create_and method" );
+  static_assert( has_create_xor_v<Ntk>, "Ntk does not implement the create_xor method" );
+  static_assert( has_get_constant_v<Ntk>, "Ntk does not implement the get_constant method" );
+
+  static_assert( std::is_same_v<std::decay_t<typename std::iterator_traits<BeginIter>::value_type>, signal<Ntk>>, "BeginIter value_type must be Ntk signal type" );
+  static_assert( std::is_same_v<std::decay_t<typename std::iterator_traits<EndIter>::value_type>, signal<Ntk>>, "EndIter value_type must be Ntk signal type" );
+
   using signal = typename Ntk::signal;
 
   std::vector<signal> signals;
@@ -623,14 +755,7 @@ void insert( Ntk& ntk, BeginIter begin, EndIter end, xag_index_list const& indic
     signal const s0 = ( lit0 % 2 ) ? !signals.at( i0 ) : signals.at( i0 );
     signal const s1 = ( lit1 % 2 ) ? !signals.at( i1 ) : signals.at( i1 );
 
-    if ( lit0 < lit1 )
-    {
-      signals.push_back( ntk.create_and( s0, s1 ) );
-    }
-    else if ( lit0 > lit1 )
-    {
-      signals.push_back( ntk.create_xor( s0, s1 ) );
-    }
+    signals.push_back( lit0 < lit1 ? ntk.create_and( s0, s1 ) : ntk.create_xor( s0, s1 ) );
   });
 
   indices.foreach_po( [&]( uint32_t lit ){
@@ -651,6 +776,10 @@ void insert( Ntk& ntk, BeginIter begin, EndIter end, xag_index_list const& indic
 template<typename Ntk, typename IndexList>
 void decode( Ntk& ntk, IndexList const& indices )
 {
+  static_assert( is_network_type_v<Ntk>, "Ntk is not a network type" );
+  static_assert( has_create_pi_v<Ntk>, "Ntk does not implement the create_pi method" );
+  static_assert( has_create_po_v<Ntk>, "Ntk does not implement the create_po method" );
+
   using signal = typename Ntk::signal;
 
   std::vector<signal> signals;
