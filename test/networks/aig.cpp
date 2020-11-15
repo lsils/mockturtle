6fb481f5e8bb3f96787cfbf0187965b704406134
Heinz Riener <heinz.riener@gmail.com>
2020-11-15 17:01:12 +0100

    comments.

--- test/networks/aig.cpp
+++ test/networks/aig.cpp
@@ -784,7 +784,7 @@ TEST_CASE( "substitute node by constant in NAND-based XOR circuit (test case 2)"
   CHECK( aig.fanout_size( aig.get_node( f4 ) ) == 1u );
 }
 
-TEST_CASE( "remove node from AIG", "[aig]" )
+TEST_CASE( "invoke take_out_node two times on the same node", "[aig]" )
 {
   aig_network aig;
   const auto x1 = aig.create_pi();
@@ -800,15 +800,13 @@ TEST_CASE( "remove node from AIG", "[aig]" )
   /* delete node */
   CHECK( !aig.is_dead( aig.get_node( f1 ) ) );
   aig.take_out_node( aig.get_node( f1 ) );
-
   CHECK( aig.is_dead( aig.get_node( f1 ) ) );
   CHECK( aig.fanout_size( aig.get_node( x1 ) ) == 1u );
   CHECK( aig.fanout_size( aig.get_node( x2 ) ) == 1u );
 
-  /* ensure that double deletion has no affect on x1 and x2 */
+  /* ensure that double-deletion has no effect on the fanout-size of x1 and x2 */
   CHECK( aig.is_dead( aig.get_node( f1 ) ) );
   aig.take_out_node( aig.get_node( f1 ) );
-
   CHECK( aig.is_dead( aig.get_node( f1 ) ) );
   CHECK( aig.fanout_size( aig.get_node( x1 ) ) == 1u );
   CHECK( aig.fanout_size( aig.get_node( x2 ) ) == 1u );
