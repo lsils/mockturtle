d8b1d8e3e2f65627dbe0160d66d4cfb333dfc9b2
Heinz Riener <heinz.riener@gmail.com>
2020-11-25 19:56:16 +0100

    runtime profiling.

--- include/mockturtle/algorithms/window_rewriting.hpp
+++ include/mockturtle/algorithms/window_rewriting.hpp
@@ -49,6 +49,8 @@ struct window_rewriting_params
 {
   uint64_t cut_size{6};
   uint64_t num_levels{5};
+
+  bool filter_cyclic_substitutions{false};
 }; /* window_rewriting_params */
 
 struct window_rewriting_stats
@@ -56,8 +58,56 @@ struct window_rewriting_stats
   /*! \brief Total runtime. */
   stopwatch<>::duration time_total{0};
 
+  /*! \brief Time for constructing windows. */
+  stopwatch<>::duration time_window{0};
+
+  /*! \brief Time for optimizing windows. */
+  stopwatch<>::duration time_optimize{0};
+
+  /*! \brief Time for substituting. */
+  stopwatch<>::duration time_substitute{0};
+
+  /*! \brief Time for updating level information. */
+  stopwatch<>::duration time_levels{0};
+
+  /*! \brief Time for updating window outputs. */
+  stopwatch<>::duration time_update_vector{0};
+
+  /*! \brief Time for topological sorting. */
+  stopwatch<>::duration time_topo_sort{0};
+
+  /*! \brief Time for encoding index_list. */
+  stopwatch<>::duration time_encode{0};
+
   /*! \brief Total number of calls to the resub. engine. */
   uint64_t num_substitutions{0};
+  uint64_t num_restrashes{0};
+  uint64_t num_windows{0};
+  uint64_t gain{0};
+
+  void report() const
+  {
+    stopwatch<>::duration time_other =
+      time_total - time_window - time_topo_sort - time_optimize - time_substitute - time_levels - time_update_vector;
+
+    fmt::print( "===========================================================================\n" );
+    fmt::print( "[i] Windowing =  {:7.2f} ({:5.2f}%) (#win = {})\n",
+                to_seconds( time_window ), to_seconds( time_window ) / to_seconds( time_total ) * 100, num_windows );
+    fmt::print( "[i] Top.sort =   {:7.2f} ({:5.2f}%)\n", to_seconds( time_topo_sort ), to_seconds( time_topo_sort ) / to_seconds( time_total ) * 100 );
+    fmt::print( "[i] Enc.list =   {:7.2f} ({:5.2f}%)\n", to_seconds( time_encode ), to_seconds( time_encode ) / to_seconds( time_total ) * 100 );
+    fmt::print( "[i] Optimize =   {:7.2f} ({:5.2f}%) (#resubs = {}, est. gain = {})\n",
+                to_seconds( time_optimize ), to_seconds( time_optimize ) / to_seconds( time_total ) * 100, num_substitutions, gain );
+    fmt::print( "[i] Substitute = {:7.2f} ({:5.2f}%) (#hash upd. = {})\n",
+                to_seconds( time_substitute ),
+                to_seconds( time_substitute ) / to_seconds( time_total ) * 100,
+                num_restrashes );
+    fmt::print( "[i] Upd.levels = {:7.2f} ({:5.2f}%)\n", to_seconds( time_levels ), to_seconds( time_levels ) / to_seconds( time_total ) * 100 );
+    fmt::print( "[i] Upd.win =    {:7.2f} ({:5.2f}%)\n", to_seconds( time_update_vector ), to_seconds( time_update_vector ) / to_seconds( time_total ) * 100 );
+    fmt::print( "[i] Other =      {:7.2f} ({:5.2f}%)\n", to_seconds( time_other ), to_seconds( time_other ) / to_seconds( time_total ) * 100 );
+    fmt::print( "---------------------------------------------------------------------------\n" );
+    fmt::print( "[i] TOTAL =      {:7.2f}\n", to_seconds( time_total ) );
+    fmt::print( "===========================================================================\n" );
+  }
 }; /* window_rewriting_stats */
 
 namespace detail
@@ -95,9 +145,9 @@ bool is_contained_in_tfi_recursive( Ntk const& ntk, typename Ntk::node const& no
 template<typename Ntk>
 bool is_contained_in_tfi( Ntk const& ntk, typename Ntk::node const& node, typename Ntk::node const& n )
 {
-  /* do not even build the TFI and just search for the node */
+  /* do not even build the TFI, but just search for the node */
   ntk.new_color();
-  return is_contained_in_tfi_recursive( ntk, node, n );
+  return detail::is_contained_in_tfi_recursive( ntk, node, n );
 }
 
 namespace detail
@@ -150,13 +200,20 @@ public:
         continue;
       }
 
-      if ( const auto w = windowing.run( n, ps.cut_size, ps.num_levels ) )
+      if ( const auto w = call_with_stopwatch( st.time_window, [&]() { return windowing.run( n, ps.cut_size, ps.num_levels ); } ) )
       {
-        window_view win( ntk, w->inputs, w->outputs, w->nodes );
-        topo_view topo_win{win};
+        ++st.num_windows;
+
+        auto topo_win = call_with_stopwatch( st.time_topo_sort, ( [&](){
+          window_view win( ntk, w->inputs, w->outputs, w->nodes );
+          topo_view topo_win{win};
+          return topo_win;
+        }) );
 
         abc_index_list il;
-        encode( il, topo_win );
+        call_with_stopwatch( st.time_encode, [&]() {
+          encode( il, topo_win );
+        } );
 
         auto il_opt = optimize( il );
         if ( !il_opt )
@@ -190,7 +247,7 @@ public:
 
                   /* ensure that _old is not in the TFI of _new */
                   // assert( !is_contained_in_tfi( ntk, ntk.get_node( _new ), ntk.get_node( _old ) ) );
-                  if ( is_contained_in_tfi( ntk, ntk.get_node( _new ), ntk.get_node( _old ) ) )
+                  if ( ps.filter_cyclic_substitutions && is_contained_in_tfi( ntk, ntk.get_node( _new ), ntk.get_node( _old ) ) )
                   {
                     std::cout << "undo resubstitution " << ntk.get_node( _old ) << std::endl;
                     ntk.take_out_node( ntk.get_node( _new ) );
@@ -203,7 +260,7 @@ public:
                 });
 
         /* recompute levels and depth */
-        ntk.update_levels();
+        call_with_stopwatch( st.time_levels, [&]() { ntk.update_levels(); } );
 
         /* ensure that no dead nodes are reachable */
         assert( count_reachable_dead_nodes( ntk ) == 0u );
@@ -226,6 +283,8 @@ private:
   /* optimize an index_list and return the new list */
   std::optional<abc_index_list> optimize( abc_index_list const& il, bool verbose = false )
   {
+    stopwatch t( st.time_optimize );
+
     int *raw = ABC_CALLOC( int, il.size() + 1u );
     uint64_t i = 0;
     for ( auto const& v : il.raw() )
@@ -245,6 +304,7 @@ private:
       fmt::print( "Performed resub {} times.  Reduced {} nodes.\n",
                   num_resubs, new_entries > 0 ? ( ( il.size() / 2u ) - new_entries ) : 0 );
     }
+    st.gain += new_entries > 0 ? ( ( il.size() / 2u ) - new_entries ) : 0;
 
     if ( raw )
     {
@@ -275,6 +335,8 @@ private:
   /* substitute the node with a signal and return all strashing updates */
   std::vector<std::pair<node, signal>> substitute_node( node const& old_node, signal const& new_signal )
   {
+    stopwatch t( st.time_substitute );
+
     std::vector<std::pair<node, signal>> updates;
     std::stack<std::pair<node, signal>> to_substitute;
     to_substitute.push( {old_node, new_signal} );
@@ -297,6 +359,7 @@ private:
         if ( const auto repl = ntk.replace_in_node( idx, _old, _new ); repl )
         {
           to_substitute.push( *repl );
+          ++st.num_restrashes;
         }
       }
 
@@ -312,6 +375,8 @@ private:
 
   void update_vector( std::vector<signal>& vs, std::vector<std::pair<node, signal>> const& updates )
   {
+    stopwatch t( st.time_update_vector );
+
     for ( auto it = std::begin( vs ); it != std::end( vs ); ++it )
     {
       for ( auto it2 = std::begin( updates ); it2 != std::end( updates ); ++it2 )
