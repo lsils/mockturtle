8a837fd988065689559be3263ff88dbf3519ae12
Heinz Riener <heinz.riener@gmail.com>
2021-04-10 21:12:56 +0200

    remove unreachable code.

--- include/mockturtle/algorithms/aig_resub.hpp
+++ include/mockturtle/algorithms/aig_resub.hpp
@@ -330,11 +330,12 @@ public:
 
       if ( true ) // ( ps.fix_bug )
       {
-        if ( kitty::implies( ~tt_d, tt ) )
-        {
-          udivs.positive_divisors.emplace_back( !ntk.make_signal( d ) );
-          continue;
-        }
+        /* unreachable case */
+        // if ( kitty::implies( ~tt_d, tt ) )
+        // {
+        //   udivs.positive_divisors.emplace_back( !ntk.make_signal( d ) );
+        //   continue;
+        // }
         if ( kitty::implies( tt, ~tt_d ) )
         {
           udivs.negative_divisors.emplace_back( !ntk.make_signal( d ) );
