651e5bbb6c726ef02e9bc6c256c2be6d1ba791f1
Heinz Riener <heinz.riener@gmail.com>
2021-04-27 19:37:49 +0200

    test case.

--- test/algorithms/resyn_engines/xag_resyn_engines.cpp
+++ test/algorithms/resyn_engines/xag_resyn_engines.cpp
@@ -125,3 +125,88 @@ TEST_CASE( "AIG resynthesis -- recursive", "[xag_resyn]" )
   kitty::create_from_binary_string( tts[5], "0000000011111101" ); // binate
   test_aig_kresub( target, tts, 5 ); // (5 & 6) | ( ~(2 & 4) & (1 | 3) )
 }
+
+template<uint32_t num_vars>
+class simulator
+{
+public:
+  explicit simulator( std::vector<kitty::static_truth_table<num_vars>> const& input_values )
+    : input_values_( input_values )
+  {}
+
+  kitty::static_truth_table<num_vars> compute_constant( bool value ) const
+  {
+    kitty::static_truth_table<num_vars> tt;
+    return value ? ~tt : tt;
+  }
+
+  kitty::static_truth_table<num_vars> compute_pi( uint32_t index ) const
+  {
+    assert( index < input_values_.size() );
+    return input_values_[index];
+  }
+
+  kitty::static_truth_table<num_vars> compute_not( kitty::static_truth_table<num_vars> const& value ) const
+  {
+    return ~value;
+  }
+
+private:
+  std::vector<kitty::static_truth_table<num_vars>> const& input_values_;
+}; /* simulator */
+
+TEST_CASE( "Synthesize AIGs for all 3-input functions", "[xag_resyn]" )
+{
+  constexpr uint32_t const num_vars = 3u;
+  using truth_table_type = kitty::static_truth_table<num_vars>;
+
+  xag_resyn_engine_stats st;
+  xag_resyn_engine_params ps;
+  ps.max_size = std::numeric_limits<uint32_t>::max();
+
+  std::vector<truth_table_type> divisor_functions;
+  truth_table_type x;
+  for ( uint32_t i = 0; i < num_vars; ++i )
+  {
+    kitty::create_nth_var( x, i );
+    divisor_functions.emplace_back( x );
+  }
+
+  truth_table_type target, care;
+  care = ~target.construct();
+
+  uint32_t success_counter{0};
+  uint32_t failed_counter{0};
+  do
+  {
+    xag_resyn_engine<truth_table_type, aig_network, uint32_t, std::vector<truth_table_type>, true, true>
+      engine( target, care, divisor_functions, st, ps );
+    for ( auto i = 0u; i < divisor_functions.size(); ++i )
+    {
+      engine.add_divisor( i );
+    }
+
+    auto const index_list = engine();
+    if ( index_list )
+    {
+      ++success_counter;
+
+      /* verify index_list using simulation */
+      aig_network aig;
+      decode( aig, *index_list );
+
+      simulator<num_vars> sim( divisor_functions );
+      auto const tts = simulate<truth_table_type, aig_network>( aig, sim );
+      CHECK( target == tts[0] );
+    }
+    else
+    {
+      ++failed_counter;
+    }
+
+    kitty::next_inplace( target );
+  } while ( !kitty::is_const0( target ) );
+
+  CHECK( success_counter == 254 );
+  CHECK( failed_counter == 2 );
+}
