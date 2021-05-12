835d0d5002c66da30fe9264cc09e32b683a88d6e
Heinz Riener <heinz.riener@gmail.com>
2021-05-12 16:09:27 +0200

    simplify.

--- test/algorithms/aqfp_resynthesis/dag_gen_error.cpp
+++ test/algorithms/aqfp_resynthesis/dag_gen_error.cpp
@@ -1,5 +1,6 @@
 #include <catch.hpp>
 
+#include <chrono>
 #include <optional>
 #include <set>
 
@@ -10,79 +11,43 @@
 
 using namespace mockturtle;
 
-TEST_CASE( "join a threads", "[thread]" )
+void f1()
 {
-  std::thread thread( []( auto ) {}, 0 );
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+}
 
+TEST_CASE( "join a threads (without lambda)", "[thread]" )
+{
+  std::thread thread( f1 );
   if ( thread.joinable() )
   {
     std::cout << "thread is joinable" << std::endl;
   }
-
   std::cout << "try to join" << std::endl;  
   thread.join();
   std::cout << "finished joining" << std::endl;
 }
 
-TEST_CASE( "join on threads in vector", "[thread]" )
+TEST_CASE( "join a threads (without args)", "[thread]" )
 {
-  uint32_t const num_threads{1};
-
-  std::vector<std::thread> threads;
-  for ( auto i = 0u; i < num_threads; i++ )
+  std::thread thread( []() {} );
+  if ( thread.joinable() )
   {
-    std::cout << "emplace thread " << i << std::endl;
-    threads.emplace_back( []( auto id ) {}, i );
-    std::cout << "finished emplacing thread " << i << std::endl;
+    std::cout << "thread is joinable" << std::endl;
   }
+  std::cout << "try to join" << std::endl;  
+  thread.join();
+  std::cout << "finished joining" << std::endl;
+}
 
-  for ( auto i = 0u; i < num_threads; i++ )
-  {
-    if ( threads[i].joinable() )
-    {
-      std::cout << "thread " << i << " is joinable" << std::endl;
-    }
-
-    
-    std::cout << "join thread " << i << std::endl;
-    try
-    {
-      threads[i].join();
-    }
-    catch ( const std::system_error& e )
-    {
-      std::cout << e.what() << '\n';
-      std::cout << e.code() << '\n';
-    }
-    catch ( const std::exception& e )
-    {
-      std::cout << e.what() << '\n';
-    }
-    std::cout << "finished joining thread " << i << std::endl;
-  }
-}  
-
-TEST_CASE( "DAG generation", "[aqfp_resyn]" )
+TEST_CASE( "join a thread (with args)", "[thread]" )
 {
-  using Ntk = mockturtle::aqfp_dag<>;
-
-  mockturtle::dag_generator_params params;
-
-  params.max_gates = 3u;         // allow at most 3 gates in total
-  params.max_num_fanout = 1000u; // limit the maximum fanout of a gate
-  params.max_width = 1000u;      // maximum number of gates at any level
-  params.max_num_in = 4u;        // maximum number of inputs slots (need extra one for the constant)
-  params.max_levels = 3u;        // maximum number of gate levels in a DAG
-
-  params.allowed_num_fanins = { 3u, 5u };
-  params.max_gates_of_fanin = { { 3u, 3u }, { 5u, 1u } };
-
-  params.verbose = true;
-
-  std::vector<Ntk> generated_dags;
-
-  mockturtle::dag_generator<> gen( params, 1u );
-  gen.for_each_dag( [&]( const auto& dag, auto thread_id ) { (void)thread_id; generated_dags.push_back( dag ); } );
-
-  CHECK( generated_dags.size() == 3018u );
+  std::thread thread( []( auto ) {}, 0 );
+  if ( thread.joinable() )
+  {
+    std::cout << "thread is joinable" << std::endl;
+  }
+  std::cout << "try to join" << std::endl;  
+  thread.join();
+  std::cout << "finished joining" << std::endl;
 }
