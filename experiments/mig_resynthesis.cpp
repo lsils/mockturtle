7735e38191a338b3a7713c92c1dbc1084bca5e0b
Siang-Yun (Sonia) Lee <siang-yun.lee@epfl.ch>
2021-10-07 15:14:51 +0200

    Update mig_resynthesis.cpp

--- experiments/mig_resynthesis.cpp
+++ experiments/mig_resynthesis.cpp
@@ -30,11 +30,14 @@
 #include <mockturtle/algorithms/node_resynthesis/mig_npn.hpp>
 #include <mockturtle/algorithms/mapper.hpp>
 #include <mockturtle/io/aiger_reader.hpp>
+#include <mockturtle/io/verilog_reader.hpp>
+#include <mockturtle/io/write_verilog.hpp>
 #include <mockturtle/networks/aig.hpp>
 #include <mockturtle/networks/mig.hpp>
 #include <mockturtle/views/depth_view.hpp>
 #include <mockturtle/views/fanout_view.hpp>
 #include <lorina/aiger.hpp>
+#include <lorina/verilog.hpp>
 
 #include <experiments.hpp>
 #include <fmt/format.h>
@@ -56,25 +59,40 @@ int main()
   {
     fmt::print( "[i] processing {}\n", benchmark );
 
+#if 0
     /* read in as AIG */
     aig_network aig;
     if ( lorina::read_aiger( benchmark_path( benchmark ), aiger_reader( aig ) ) != lorina::return_code::success )
     {
       continue;
     }
+    uint32_t const size_aig = aig.num_gates();
 
     /* graph mapping to convert from AIG to MIG */
     map_params map_ps;
     map_ps.skip_delay_round = true;
     map_ps.required_time = std::numeric_limits<double>::max();
     mig_network mig = map( aig, exact_lib, map_ps );
-    uint32_t const size_before = mig.num_gates();
+    uint32_t const size_mapped_mig = mig.num_gates();
+
+    write_verilog( mig, "mapped_MIGs/" + benchmark + ".v" );
+    std::cout << "  AIG size = " << size_aig << ", mapped MIG size = " << size_mapped_mig << "\n";
+#else
+    mig_network mig;
+    if ( lorina::read_verilog( "mapped_MIGs/" + benchmark + ".v", verilog_reader( mig ) ) != lorina::return_code::success )
+    {
+      continue;
+    }
+    uint32_t const size_mapped_mig = mig.num_gates();
+#endif
 
     /* resubstitution using heuristic MIG resynthesis */
     resubstitution_params ps;
     resubstitution_stats st;
     ps.max_pis = 8u;
     ps.max_inserts = 1u;
+    ps.pattern_filename = "patterns/" + benchmark + ".pat";
+    ps.save_patterns = "patterns/" + benchmark + ".pat";
 
     depth_view depth_mig{mig};
     fanout_view fanout_mig{depth_mig};
@@ -93,7 +111,7 @@ int main()
     mig = cleanup_dangling( mig );
 
     bool const cec = benchmark == "hyp" ? true : abc_cec( fanout_mig, benchmark );
-    exp( benchmark, size_before, mig.num_gates(), to_seconds( st.time_total ), cec );
+    exp( benchmark, size_mapped_mig, mig.num_gates(), to_seconds( st.time_total ), cec );
   }
 
   exp.save();
