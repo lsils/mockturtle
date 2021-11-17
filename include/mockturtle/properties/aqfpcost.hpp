ceaeb55f27c861807cb46ac6800793d5c8ba5a36
Dewmini Sudara <mdsudara@gmail.com>
2021-11-17 01:39:59 +0100

    Update AQFP cost computation 	- Can handle PIs differently 	- Avoid using fanout_view as it ignores duplicated fanins (problematic for majority-5 gates)

--- include/mockturtle/properties/aqfpcost.hpp
+++ include/mockturtle/properties/aqfpcost.hpp
@@ -33,7 +33,10 @@
 #pragma once
 
 #include <algorithm>
+#include <iostream>
 #include <limits>
+#include <unordered_map>
+#include <vector>
 
 #include "../utils/hash_functions.hpp"
 #include "../views/fanout_view.hpp"
@@ -42,25 +45,32 @@ namespace mockturtle
 {
 
 /*! \brief Cost function for computing the best splitter and buffer cost for a fanout net with given relative levels. */
-class balanced_fanout_net_cost
+class fanout_net_cost
 {
 public:
   static constexpr double IMPOSSIBLE = std::numeric_limits<double>::infinity();
 
-  balanced_fanout_net_cost( const std::unordered_map<uint32_t, double>& splitters )
+  fanout_net_cost( const std::unordered_map<uint32_t, double>& splitters )
       : buffer_cost( splitters.at( 1u ) ), splitters( remove_buffer( splitters ) )
   {
   }
 
   double operator()( const std::vector<uint32_t>& config )
   {
-    return cost_for_config( config );
+    return cost_for_config( config, false );
+  }
+
+  double operator()( const std::vector<uint32_t>& config, bool ignore_initial_buffers )
+  {
+    return cost_for_config( config, ignore_initial_buffers );
   }
 
 private:
+  using cache_key_t = std::tuple<bool, std::vector<uint32_t>>;
+
   double buffer_cost;
   std::unordered_map<uint32_t, double> splitters;
-  std::unordered_map<std::vector<uint32_t>, double, hash<std::vector<uint32_t>>> cache;
+  std::unordered_map<cache_key_t, double, hash<cache_key_t>> cache;
 
   static std::unordered_map<uint32_t, double> remove_buffer( std::unordered_map<uint32_t, double> splitters )
   {
@@ -68,13 +78,13 @@ private:
     return splitters;
   }
 
-  double cost_for_config( const std::vector<uint32_t> config )
+  double cost_for_config( const std::vector<uint32_t> config, bool ignore_initial_buffers )
   {
     if ( config.size() == 1 )
     {
       if ( config[0] >= 1 )
       {
-        return ( config[0] - 1 ) * buffer_cost;
+        return ignore_initial_buffers ? 0.0 : ( config[0] - 1 ) * buffer_cost;
       }
       else
       {
@@ -82,9 +92,10 @@ private:
       }
     }
 
-    if ( cache.count( config ) )
+    std::tuple key = { ignore_initial_buffers, config };
+    if ( cache.count( key ) )
     {
-      return cache[config];
+      return cache[key];
     }
 
     auto result = IMPOSSIBLE;
@@ -110,7 +121,7 @@ private:
         new_config.push_back( sp_lev );
         std::sort( new_config.begin(), new_config.end() );
 
-        temp += cost_for_config( new_config );
+        temp += cost_for_config( new_config, ignore_initial_buffers );
 
         if ( temp < result )
         {
@@ -119,7 +130,7 @@ private:
       }
     }
 
-    return ( cache[config] = result );
+    return ( cache[key] = result );
   }
 };
 
@@ -131,51 +142,82 @@ struct aqfp_network_cost
 {
   static constexpr double IMPOSSIBLE = std::numeric_limits<double>::infinity();
 
-  aqfp_network_cost( const std::unordered_map<uint32_t, double>& gate_costs, const std::unordered_map<uint32_t, double>& splitters )
-      : gate_costs( gate_costs ), fanout_cc( splitters ) {}
+  aqfp_network_cost( const std::unordered_map<uint32_t, double>& gate_costs, const std::unordered_map<uint32_t, double>& splitters,
+                     bool balance_pis = false, bool branch_pis = false, bool balance_pos = true )
+      : gate_costs( gate_costs ), fanout_cc( splitters ), balance_pis( balance_pis ), branch_pis( branch_pis ), balance_pos( balance_pos ) {}
 
-  template<typename Ntk, typename LevelMap>
-  double operator()( const Ntk& ntk, const LevelMap& level_of_node, uint32_t critical_po_level )
+  template<typename Ntk, typename LevelMap, typename PoLevelMap>
+  double operator()( const Ntk& ntk, const LevelMap& level_of_node, const PoLevelMap& po_level_of_node )
   {
-    fanout_view dest_fv{ ntk };
+    /*  Collect fanouts of each node. 
+        Cannot use the fanout_view as duplicate fanis are not accounted with the correct multiplicity. */
+    std::unordered_map<node<Ntk>, std::vector<node<Ntk>>> fanouts;
+    ntk.foreach_gate( [&]( auto n ) {
+      ntk.foreach_fanin(n, [&](auto fi){
+        fanouts[ntk.get_node(fi)].push_back(n);
+      });
+    } );
+
     auto gate_cost = 0.0;
     auto fanout_net_cost = 0.0;
 
-    std::vector<node<Ntk>> internal_nodes;
-    dest_fv.foreach_node( [&]( auto n ) {
-      if ( dest_fv.is_constant( n ) || dest_fv.is_pi( n ) )
-      {
-        return;
-      }
-
-      if ( n > 0u && dest_fv.is_maj( n ) )
-      {
-        internal_nodes.push_back( n );
-      }
-    } );
-
-    for ( auto n : internal_nodes )
+    std::vector<node<Ntk>> nodes;
+    if ( branch_pis )
     {
-      gate_cost += gate_costs.at( ntk.fanin_size( n ) );
+      ntk.foreach_ci( [&]( auto n ) { nodes.push_back( n ); } );
+    }
+    ntk.foreach_gate( [&]( auto n ) { nodes.push_back( n ); } );
+
+    if ( po_level_of_node.size() == 0 ) {
+      std::cout << "[w] - the map po_level_of_node is empty!\n";
+    }
+
+    size_t critical_po_level = std::max_element( po_level_of_node.begin(), po_level_of_node.end(), []( auto n1, auto n2 ) { return n1.second < n2.second; } )->second;
+    for ( auto n : nodes )
+    {
+      if ( !ntk.is_ci( n ) ) // n must be a gate
+      {
+        gate_cost += gate_costs.at( ntk.fanin_size( n ) );
+      }
+
+      if (ntk.fanout_size(n) == 0) {
+        std::cout << fmt::format( "[w] - dangling node {}\n", n );
+        continue;
+      }
 
       std::vector<uint32_t> rellev;
 
-      dest_fv.foreach_fanout( n, [&]( auto fo ) {
+      for (auto fo : fanouts[n]) {
         assert( level_of_node.at( fo ) > level_of_node.at( n ) );
         rellev.push_back( level_of_node.at( fo ) - level_of_node.at( n ) );
-      } );
+      }
 
       uint32_t pos = 0u;
-      while ( rellev.size() < dest_fv.fanout_size( n ) )
+      while ( rellev.size() < ntk.fanout_size( n ) )
       {
         pos++;
-        rellev.push_back( critical_po_level - level_of_node.at( n ) );
+        if ( balance_pos )
+        {
+          rellev.push_back( critical_po_level + 1 - level_of_node.at( n ) );
+        }
+        else
+        {
+          rellev.push_back( po_level_of_node.at( n ) + 1 - level_of_node.at( n ) );
+        }
       }
 
       if ( rellev.size() > 1u || ( rellev.size() == 1u && rellev[0] > 0 ) )
       {
         std::sort( rellev.begin(), rellev.end() );
-        fanout_net_cost += fanout_cc( rellev );
+        auto net_cost = fanout_cc( rellev, ntk.is_ci( n ) && !balance_pis );
+        if (net_cost == std::numeric_limits<double>::infinity()) {
+          std::cerr << fmt::format("[e] impossible to synthesize fanout net of node {} for relative levels [{}]\n", n, fmt::join(rellev, " "));
+          std::abort();
+        }
+        fanout_net_cost += net_cost;
+      } else {
+        std::cerr << fmt::format("[e] invalid level assignment for node {} with levels [{}]\n", n, fmt::join(rellev, " "));
+        std::abort();
       }
     }
 
@@ -184,7 +226,8 @@ struct aqfp_network_cost
 
 private:
   std::unordered_map<uint32_t, double> gate_costs;
-  balanced_fanout_net_cost fanout_cc;
+  fanout_net_cost fanout_cc;
+  bool balance_pis, branch_pis, balance_pos;
 };
 
 } // namespace mockturtle
