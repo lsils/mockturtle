73c804c99bf34195ef9c70219f77ad44eaf107c3
Marcel Walter <marcelwalter.mw@gmail.com>
2022-02-14 11:09:25 +0100

    Added a test case using bill with the goal to showcase problematic Windows compilation behavior

---
+++ test/algorithms/bill_glucose_compilation_error.cpp
@@ -0,0 +1,24 @@
+//
+// Created by marcel on 14.02.22.
+//
+
+#include <catch.hpp>
+
+#include <bill/sat/solver.hpp>
+#include <bill/sat/tseytin.hpp>
+
+TEST_CASE( "Glucose compilation problem", "[glucose]" )
+{
+  bill::solver<bill::solvers::ghack> solver{}; // not actually using Glucose
+
+  // example from bill's README
+  auto const a = bill::lit_type( solver.add_variable(), bill::lit_type::polarities::positive );
+  auto const b = bill::lit_type( solver.add_variable(), bill::lit_type::polarities::positive );
+
+  auto const t0 = bill::add_tseytin_and( solver, a, b );
+  auto const t1 = ~bill::add_tseytin_or( solver, ~a, ~b );
+  auto const t2 = add_tseytin_xor( solver, t0, t1 );
+  solver.add_clause( t2 );
+
+  CHECK( solver.solve() == bill::result::states::unsatisfiable );
+}
