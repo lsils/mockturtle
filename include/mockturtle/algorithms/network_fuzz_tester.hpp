344a32cf44c8d5ec8ab0f8c2f96a199560ffe933
Siang-Yun (Sonia) Lee <siang-yun.lee@epfl.ch>
2022-04-07 14:07:20 +0200

    fix popen pclose on Windows

--- include/mockturtle/algorithms/network_fuzz_tester.hpp
+++ include/mockturtle/algorithms/network_fuzz_tester.hpp
@@ -305,7 +305,11 @@ private:
 
     std::array<char, 128> buffer;
     std::string result;
+    #ifdef _MSC_VER
+    std::unique_ptr<FILE, decltype( &pclose )> pipe( _popen( command.c_str(), "r" ), _pclose );
+    #else
     std::unique_ptr<FILE, decltype( &pclose )> pipe( popen( command.c_str(), "r" ), pclose );
+    #endif
     if ( !pipe )
     {
       throw std::runtime_error( "popen() failed" );
