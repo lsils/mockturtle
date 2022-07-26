664ddba83ba9810842256db8ffe905310b3d9eae
Nozidoali <nozidoali@gmail.com>
2022-07-26 14:46:40 +0200

    update(cec): cleanup

--- experiments/equivalence_checking.cpp
+++ experiments/equivalence_checking.cpp
@@ -53,9 +53,6 @@ int main()
 
   for ( auto const& benchmark : epfl_benchmarks() )
   {
-    if ( benchmark == "hyp" ) continue;
-    if ( benchmark == "div" ) continue;
-    // if ( benchmark != "sin" ) continue;
     fmt::print( "[i] processing {}\n", benchmark );
     aig_network aig1;
     if ( lorina::read_aiger( benchmark_path( benchmark ), aiger_reader( aig1 ) ) != lorina::return_code::success )
@@ -64,30 +61,12 @@ int main()
     }
     
     aig_network aig2;
-    if ( false )
-    {
-      klut_network klut;
-      if ( lorina::read_blif( "../experiments/cec_benchmarks/"+benchmark+".blif", blif_reader( klut ) ) != lorina::return_code::success )
-      {
-        continue;
-      }
-      aig2 = convert_klut_to_graph<aig_network, klut_network>( klut );
-    }
-    else
-    {
-      aig_network aig = aig1;
-      xag_npn_resynthesis<aig_network> resyn;
-      cut_rewriting_params ps;
-      ps.cut_enumeration_ps.cut_size = 4;
-      ps.progress = true;
-      aig2 = cut_rewriting( aig, resyn, ps );
-    }
-    if ( false )
-    {
-      aig_network miter_ = *reduced_miter<aig_network>( aig1, aig2 );
-      write_aiger( miter_, "../experiments/miters/" + benchmark + "_miter.aig" );
-      continue;
-    }
+    aig_network aig = aig1;
+    xag_npn_resynthesis<aig_network> resyn;
+    cut_rewriting_params ps;
+    ps.cut_enumeration_ps.cut_size = 4;
+    ps.progress = true;
+    aig2 = cut_rewriting( aig, resyn, ps );
 
     stopwatch<>::duration time_fast_cec{ 0 };
     fast_cec_stats cst;
@@ -96,7 +75,6 @@ int main()
     bool cec_fast = call_with_stopwatch( time_fast_cec, [&]() { 
       aig_network miter_ = *reduced_miter<aig_network>( aig1, aig2 );
       fmt::print("[i] miter #gates = {}\n", miter_.num_gates() );
-
       return *fast_cec( miter_, cps, &cst ); 
     } );
 
