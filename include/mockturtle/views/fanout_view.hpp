a1d39b9d7839980cdfba7bff156df5d3b623073b
Siang-Yun (Sonia) Lee <siang-yun.lee@epfl.ch>
2023-01-11 17:59:17 +0100

    port fix to xag,mig,xmg,tig,fanout_view

--- include/mockturtle/views/fanout_view.hpp
+++ include/mockturtle/views/fanout_view.hpp
@@ -175,11 +175,20 @@ public:
       to_substitute.pop();
 
       signal _new = _curr;
-      while ( Ntk::is_dead( Ntk::get_node( _new ) ) )
+      /* find the real new node */
+      if ( Ntk::is_dead( Ntk::get_node( _new ) ) )
       {
-        const auto it = old_to_new.find( Ntk::get_node( _new ) );
-        assert( it != old_to_new.end() );
-        _new = Ntk::is_complemented( _new ) ? Ntk::create_not( it->second ) : it->second;
+        auto it = old_to_new.find( Ntk::get_node( _new ) );
+        while ( it != old_to_new.end() )
+        {
+          _new = Ntk::is_complemented( _new ) ? Ntk::create_not( it->second ) : it->second;
+          it = old_to_new.find( Ntk::get_node( _new ) );
+        }
+      }
+      /* revive */
+      if ( Ntk::is_dead( Ntk::get_node( _new ) ) )
+      {
+        Ntk::revive_node( Ntk::get_node( _new ) );
       }
 
       if ( Ntk::get_node( _new ) == _old && !Ntk::is_complemented( _new ) )
