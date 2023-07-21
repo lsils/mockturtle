cc89b296a7232e4ceea5a00e0324a60046f0c5c4
nozidoali <nozidoali@gmail.com>
2023-07-21 11:51:27 +0200

    bug fix: delete the setups for DAC exps

--- experiments/cost_generic_resub.cpp
+++ experiments/cost_generic_resub.cpp
@@ -33,7 +33,6 @@
 #include <mockturtle/algorithms/cut_rewriting.hpp>
 #include <mockturtle/algorithms/node_resynthesis/xag_minmc2.hpp>
 #include <mockturtle/io/aiger_reader.hpp>
-#include <mockturtle/io/write_verilog.hpp>
 #include <mockturtle/utils/cost_functions.hpp>
 #include <mockturtle/utils/stopwatch.hpp>
 
@@ -45,17 +44,10 @@ int main()
   using namespace mockturtle::experimental;
   using namespace experiments;
 
-  std::string results_dir = "../experiments/results";
-
   /* run the actual experiments */
   experiment<std::string, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, float, bool> exp( "cost_generic_resub", "benchmark", "c1", "c2", "c3", "c4", "_c1", "_c2", "_c3", "_c4", "runtime", "cec" );
-  for ( auto const& benchmark : iwls_benchmarks() )
+  for ( auto const& benchmark : epfl_benchmarks() )
   {
-    /* skip these for "collect all" experiments */
-    // if (benchmark == "hyp") continue; 
-    // if (benchmark == "mem_ctrl") continue;
-    // if (benchmark == "log2") continue;
-    // if (benchmark == "sin") continue;
     float run_time = 0;
 
     fmt::print( "[i] processing {}\n", benchmark );
@@ -64,18 +56,16 @@ int main()
     assert( result == lorina::return_code::success );
     (void)result;
 
-    /* fraig */
-    // functional_reduction( xag );
-    // xag = cleanup_dangling( xag );
-
+    /* the cost functions for evalution */
     auto costfn_1 = xag_size_cost_function<xag_network>();
     auto costfn_2 = xag_depth_cost_function<xag_network>();
     auto costfn_3 = xag_multiplicative_complexity_cost_function<xag_network>();
     auto costfn_4 = xag_t_depth_cost_function<xag_network>();
 
-    std::string costfn_str = "mc";
+    /* the cost function for optimization */
     auto costfn = costfn_3;
 
+    /* the costs before optimization */
     auto c1 = cost_view( xag, costfn_1 ).get_cost();
     auto c2 = cost_view( xag, costfn_2 ).get_cost();
     auto c3 = cost_view( xag, costfn_3 ).get_cost();
@@ -85,35 +75,25 @@ int main()
     cost_generic_resub_stats st;
     ps.verbose = false;
     ps.rps.max_solutions = 1; /* = 1: collect one, =0: collect all */
-    // ps.rps.use_esop = true; /* true: use esop, false: no esop */
 
     stopwatch<>::duration time_tot{ 0 };
 
     future::xag_minmc_resynthesis resyn;
     call_with_stopwatch( time_tot, [&]() {
       cost_generic_resub( xag, costfn, ps, &st );
-
-      xag = cut_rewriting<xag_network, decltype( resyn ), mc_cost<xag_network>>( xag, resyn, ps, nullptr );
-
-      // xag = balancing( xag, { sop_rebalancing<xag_network>{} } );
-      // xag = balancing( xag, { esop_rebalancing<xag_network>{} } );
       xag = cleanup_dangling( xag );
     } );
 
-    write_verilog( xag, fmt::format("{}/{}_{}.v", results_dir, benchmark, costfn_str ) );
-
     run_time = to_seconds( time_tot );
 
-    // fmt::print( "{},{},{},{:>5.2f}\n", iter++, xag.num_gates(), cost_after, run_time );
-
+    /* the costs after optimization */
     auto _c1 = cost_view( xag, costfn_1 ).get_cost();
     auto _c2 = cost_view( xag, costfn_2 ).get_cost();
     auto _c3 = cost_view( xag, costfn_3 ).get_cost();
     auto _c4 = cost_view( xag, costfn_4 ).get_cost();
 
     auto cec = true;
-    // cec = benchmark == "hyp" ? true : abc_cec( xag, benchmark );
-    // cec = xag.num_gates() > 10000 ? true : abc_cec( xag, benchmark );
+    cec = benchmark == "hyp" ? true : abc_cec( xag, benchmark );
     exp( benchmark, c1, c2, c3, c4, _c1, _c2, _c3, _c4, run_time, cec );
   }
   exp.save();
