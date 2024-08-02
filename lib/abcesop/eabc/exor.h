3e56b420d07fbf3c0b624f84445af2e9af7856c7
Robert O'Callahan <rocallahan@google.com>
2024-08-02 04:31:20 +0000

    Remove support for `int` sizes other than 32 bits
    No viable platform uses anything other than 32 bits for `int`.

--- lib/abcesop/eabc/exor.h
+++ lib/abcesop/eabc/exor.h
@@ -55,7 +55,7 @@ namespace abc::exorcism {
 ///                       MACRO DEFINITIONS                          ///
 ////////////////////////////////////////////////////////////////////////
 
-// the number of bits per integer (can be 16, 32, 64 - tested for 32)
+// the number of bits per integer
 #define BPI                 32 
 #define BPIMASK             31 
 #define LOGBPI               5
@@ -71,17 +71,8 @@ namespace abc::exorcism {
 // the following number of cube pairs are allocated:
 // nCubesAlloc/CUBE_PAIR_FACTOR
 
-#if BPI == 64
-#define DIFFERENT   0x5555555555555555
-#define BIT_COUNT(w)   (BitCount[(w)&0xffff] + BitCount[((w)>>16)&0xffff] + BitCount[((w)>>32)&0xffff] + BitCount[(w)>>48])
-#elif BPI == 32
 #define DIFFERENT   0x55555555
 #define BIT_COUNT(w)   (BitCount[(w)&0xffff] + BitCount[(w)>>16])
-#else
-#define DIFFERENT   0x5555
-#define BIT_COUNT(w)   (BitCount[(w)])
-#endif
-
 
 #define VarWord(element)  ((element)>>LOGBPI)
 #define VarBit(element)   ((element)&BPIMASK)
